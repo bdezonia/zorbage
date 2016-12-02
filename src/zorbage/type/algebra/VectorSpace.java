5b2f611c64ac6cae370c3fbe3408d81de08c4d89
Barry DeZonia <bdezonia@gmail.com>
2016-12-01 22:03:15 -0600

    Define VectorSpace

---
+++ src/zorbage/type/algebra/VectorSpace.java
@@ -0,0 +1,50 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package zorbage.type.algebra;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ * @param <T> the vector space for the vectors
+ * @param <U> the vector member type for the vectors
+ * @param <V> the field for the coefficients
+ * @param <W> the scalar member type for the coefficients
+ */
+public interface VectorSpace<T extends VectorSpace<T,U,V,W>, U, V extends Field<V,W>, W>
+  extends
+    AdditiveGroup<T,U>,
+    Norm<U,W>
+{
+	void scale(W scalar, U a, U b);
+	void crossProduct(U a, U b, U c);
+	void dotProduct(U a, U b, W c);
+	void perpDotProduct(U a, U b, W c);
+	//void vectorDirectProduct(U a, U b, Tensor c); // TODO
+	void vectorTripleProduct(U a, U b, U c, U d);
+	void scalarTripleProduct(U a, U b, U c, W d);
+}
