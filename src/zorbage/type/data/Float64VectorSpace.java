7203c3f9208260dc6c8c74df50a5f29ecde20df2
Barry DeZonia <bdezonia@gmail.com>
2016-12-10 19:09:13 -0600

    Finish float64 vector space code
    Note I am unhappy with interface. I have defined multiplication
    methods that only work in some dimensions.

--- src/zorbage/type/data/Float64VectorSpace.java
+++ src/zorbage/type/data/Float64VectorSpace.java
@@ -134,10 +134,23 @@ public class Float64VectorSpace
 
 	@Override
 	public void crossProduct(Float64VectorMember a, Float64VectorMember b, Float64VectorMember c) {
-		// TODO Auto-generated method stub
-		
+		// kludgy: 3 dim only. treating lower dim vectors as having zeroes in other positions.
+		if (!compatible(3,a) || !compatible(3,b))
+			throw new UnsupportedOperationException("vector cross product defined for 3 dimensions");
+		Float64VectorMember tmp = new Float64VectorMember(new double[3]);
+		tmp.setV(0, a.v(1)*b.v(2) - a.v(2)*b.v(2));
+		tmp.setV(1, a.v(2)*b.v(0) - a.v(0)*b.v(2));
+		tmp.setV(2, a.v(0)*b.v(1) - a.v(1)*b.v(0));
+		assign(tmp, c);
 	}
 
+	private boolean compatible(int dim, Float64VectorMember v) {
+		for (int i = dim; i < v.length(); i++) {
+			if (v.v(i) != 0)
+				return false;
+		}
+		return true;
+	}
 	@Override
 	public void dotProduct(Float64VectorMember a, Float64VectorMember b, Float64Member c) {
 		int min = Math.min(a.length(), b.length());
@@ -150,22 +163,28 @@ public class Float64VectorSpace
 
 	@Override
 	public void perpDotProduct(Float64VectorMember a, Float64VectorMember b, Float64Member c) {
-		// TODO Auto-generated method stub
-		
+		// kludgy: 2 dim only. treating lower dim vectors as having zeroes in other positions.
+		if (!compatible(2,a) || !compatible(2,b))
+			throw new UnsupportedOperationException("vector perp dot product defined for 2 dimensions");
+		c.setV(-a.v(1)*b.v(0) + a.v(0)*b.v(1));
 	}
 
 	@Override
 	public void vectorTripleProduct(Float64VectorMember a, Float64VectorMember b, Float64VectorMember c,
-			Float64VectorMember d) {
-		// TODO Auto-generated method stub
-		
+			Float64VectorMember d)
+	{
+		Float64VectorMember b_cross_c = new Float64VectorMember(new double[3]);
+		crossProduct(b, c, b_cross_c);
+		crossProduct(a, b_cross_c, d);
 	}
 
 	@Override
 	public void scalarTripleProduct(Float64VectorMember a, Float64VectorMember b, Float64VectorMember c,
-			Float64Member d) {
-		// TODO Auto-generated method stub
-		
+			Float64Member d)
+	{
+		Float64VectorMember b_cross_c = new Float64VectorMember(new double[3]);
+		crossProduct(b, c, b_cross_c);
+		dotProduct(a, b_cross_c, d);
 	}
 
 }
