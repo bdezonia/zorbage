87aa3b726d863aa351dbfda4b5112374ab63d253
Barry DeZonia <bdezonia@gmail.com>
2016-12-11 22:37:22 -0600

    Define generic storage based upon a group

---
+++ src/zorbage/type/storage/ArrayStorageGeneric.java
@@ -0,0 +1,71 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package zorbage.type.storage;
+
+import zorbage.type.algebra.Group;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ * @param <U>
+ */
+public class ArrayStorageGeneric<T extends Group<T,U>,U>
+	implements Storage<U>
+{
+
+	private final Object[] data;
+	private final T g;
+	
+	public ArrayStorageGeneric(long size, T g) {
+		if (size < 0)
+			throw new IllegalArgumentException("ArrayStorage cannot handle a negative request");
+		if (size > Integer.MAX_VALUE)
+			throw new IllegalArgumentException("ArrayStorage cannot handle such a large request");
+		this.data = new Object[(int)size];
+		for (int i = 0; i < size; i++) {
+			this.data[i] = g.construct();
+		}
+		this.g = g;
+	}
+
+	@Override
+	public void put(long index, U value) {
+		g.assign(value, (U)data[(int)index]);
+	}
+
+	@Override
+	public void get(long index, U value) {
+		g.assign((U)data[(int)index], value);
+	}
+	
+	@Override
+	public long size() {
+		return data.length;
+	}
+
+}
