6e47ef139b66c8dc282b549459d4260e12d86f35
Barry DeZonia <bdezonia@gmail.com>
2017-01-07 13:49:11 -0600

    Update copyrights

--- src/zorbage/type/storage/ArrayStorageQuaternionFloat64.java
+++ src/zorbage/type/storage/ArrayStorageQuaternionFloat64.java
@@ -1,7 +1,7 @@
 /*
  * Zorbage: an algebraic data hierarchy for use in numeric processing.
  *
- * Copyright (C) 2016 Barry DeZonia
+ * Copyright (C) 2016-2017 Barry DeZonia
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
