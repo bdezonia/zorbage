342e70da5e08880e8ef4061dc79af1ace3851357
Barry DeZonia <bdezonia@gmail.com>
2017-01-22 17:07:40 -0600

    Make a stddev set of classes

---
+++ src/zorbage/type/operation/StdDevI.java
@@ -0,0 +1,55 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2017 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package zorbage.type.operation;
+
+import zorbage.type.algebra.AdditiveGroup;
+import zorbage.type.algebra.IntegralDivision;
+import zorbage.type.algebra.Invertible;
+import zorbage.type.algebra.Multiplication;
+import zorbage.type.algebra.Roots;
+import zorbage.type.algebra.Unity;
+import zorbage.type.storage.Storage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class StdDevI<T extends AdditiveGroup<T,U> & Multiplication<U> & Unity<U> & IntegralDivision<U> & Roots<U>, U> {
+
+	private T g;
+	
+	public StdDevI(T g) {
+		this.g = g;
+	}
+	
+	public void calculate(Storage<?,U> storage, U result) {
+		VarianceI<T,U> var = new VarianceI<T,U>(g);
+		var.calculate(storage, result);
+		g.sqrt(result, result);
+	}
+}
