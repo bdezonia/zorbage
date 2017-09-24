730e043095dd7de8cb9e7a3a24b59ab0c6b762bf
Barry DeZonia <bdezonia@gmail.com>
2017-09-24 11:08:50 -0500

    Add a real cartesian grid sampling

---
+++ src/zorbage/region/sampling/SamplingCartesianRealGrid.java
@@ -0,0 +1,113 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2017 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package zorbage.region.sampling;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class SamplingCartesianRealGrid implements Sampling<RealIndex> {
+
+	private final int numD;
+	private final RealIndex minPt;
+	private final RealIndex maxPt;
+	private final IntegerIndex dimCounts;
+	
+	public SamplingCartesianRealGrid(double[] point1, double[] point2, long[] counts) {
+		if ((point1.length != point2.length) || (point1.length != counts.length))
+			throw new IllegalArgumentException("mismatched dimensions of input points");
+		numD = point1.length;
+		minPt = new RealIndex(numD);
+		maxPt = new RealIndex(numD);
+		dimCounts = new IntegerIndex(numD);
+		for (int i = 0; i < numD; i++) {
+			minPt.set(i, Math.min(point1[i], point2[i]));
+			maxPt.set(i, Math.max(point1[i], point2[i]));
+			dimCounts.set(i, Math.abs(counts[i]));
+		}
+	}
+
+	@Override
+	public int numDimensions() {
+		return numD;
+	}
+	
+	@Override
+	public boolean contains(RealIndex samplePoint) {
+		if (samplePoint.numDimensions() != numD)
+			throw new IllegalArgumentException("contains() expects input point to have same dimension as sampling");
+		// TODO: do I lay on a grid point
+		throw new IllegalArgumentException("TODO - do I lay on a grid point");
+	}
+
+	@Override
+	public SamplingIterator<RealIndex> iterator() {
+		return new Iterator();
+	}
+	
+	private class Iterator implements SamplingIterator<RealIndex> {
+
+		private final IntegerIndex index;
+		
+		private Iterator() {
+			index = new IntegerIndex(numD);
+			index.set(0, index.get(0) - 1);
+		}
+		
+		@Override
+		public boolean hasNext() {
+			for (int i = 0; i < numD; i++) {
+				if (index.get(i) >= dimCounts.get(i)-1)
+					continue;
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public void next(RealIndex value) {
+			if (value.numDimensions() != numD)
+				throw new IllegalArgumentException("mismatched dimensions of output point");
+			for (int i = 0; i < numD; i++) {
+				if (index.get(i) < dimCounts.get(i)) {
+					index.set(i, index.get(i) + 1);
+					for (int j = 0; j < numD; j++) {
+						// TODO: does this visit every edge?
+						value.set(j, (maxPt.get(j) - minPt.get(j)) * index.get(j) / dimCounts.get(j));
+					}
+					return;
+				}
+				else
+					index.set(i, 0);
+			}
+			throw new IllegalArgumentException("next() called on sampling that does not hasNext()");
+		}
+		
+	}
+
+}
