c8d979697e76773e5731134c948a27c488a2a605
Barry DeZonia <bdezonia@gmail.com>
2017-10-13 20:40:16 -0500

    Redo LU Decomposition example to working code

--- src/zorbage/example/LUDecompExample.java
+++ src/zorbage/example/LUDecompExample.java
@@ -34,12 +34,6 @@ import zorbage.type.data.float64.real.Float64MatrixMember;
 import zorbage.type.data.float64.real.Float64Member;
 import zorbage.type.data.float64.real.Float64Vector;
 import zorbage.type.data.float64.real.Float64VectorMember;
-import zorbage.type.data.int64.SignedInt64Member;
-import zorbage.type.storage.linear.LinearStorage;
-import zorbage.type.storage.linear.array.ArrayStorageFloat64;
-import zorbage.type.storage.linear.array.ArrayStorageSignedInt64;
-
-// TODO: test. Not sure I got this right.
 
 /**
  * 
@@ -74,243 +68,91 @@ public class LUDecompExample {
 		
 		Float64VectorMember x = dblVec.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, 3);
 
-		if (run(a,b,0.000001,x) != -1)
-			System.out.println("Success");
-		else
-			System.out.println("Error");
-		
+		run(a,b,x);
+
 		System.out.println(x.toString());
 	}
 	
-	public int run(Float64MatrixMember a, Float64VectorMember b, double tol, Float64VectorMember x) {
+	public void run(Float64MatrixMember a, Float64VectorMember b, Float64VectorMember x) {
 		
 		final long n = x.length();
 		
-		LinearStorage<?,SignedInt64Member> o =
-				new ArrayStorageSignedInt64<SignedInt64Member>(n, new SignedInt64Member());
+        // decomposition of matrix
 
-		LinearStorage<?,Float64Member> s =
-				new ArrayStorageFloat64<Float64Member>(n, new Float64Member());
+		Float64MatrixMember lu = dblMat.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, n, n);
+        Float64Member sum = dbl.construct();
+        Float64Member value1 = dbl.construct();
+        Float64Member value2 = dbl.construct();
+        Float64Member term = dbl.construct();
+        Float64Member tmp = dbl.construct();
+        for (long i = 0; i < n; i++)
+        {
+            for (long j = i; j < n; j++)
+            {
+                sum.setV(0);
+                for (long k = 0; k < i; k++) {
+                	dbl.add(sum, term, sum);
+                    lu.v(i, k, value1);
+                    lu.v(k, j, value2);
+                    dbl.add(value1, value2, term);
+                	dbl.add(sum, term, sum);
+                }
+                a.v(i, j, term);
+                dbl.subtract(term, sum, term);
+                lu.setV(i, j, term);
+            }
+            for (long j = i + 1; j < n; j++)
+            {
+                sum.setV(0);
+                for (long k = 0; k < i; k++) {
+                	lu.v(j, k, value1);
+                	lu.v(k, i, value2);
+                	dbl.multiply(value1, value2, term);
+                	dbl.add(sum, term, sum);
+                }
+                dbl.unity(value1);
+                dbl.divide(value1, tmp, value1);
+                a.v(j, i, tmp);
+                dbl.subtract(tmp, sum, value2);
+                dbl.multiply(value1, value2, term);
+                lu.setV(j, i, term);
+            }
+        }
+        
+        // find solution of Ly = b
+        Float64VectorMember y = dblVec.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, n);
+        for (long i = 0; i < n; i++)
+        {
+            sum.setV(0);
+            for (long k = 0; k < i; k++) {
+            	lu.v(i, k, value1);
+            	y.v(k, value2);
+            	dbl.multiply(value1, value2, term);
+            	dbl.add(sum, term, sum);
+            }
+            b.v(i, value1);
+            dbl.subtract(value1, sum, term);
+            y.setV(i, term);
+        }
 
-		if (decompose(a,tol,o,s) != -1) {
-			substitute(a,o,b,x);
-			return 0;
-		} else {
-			return -1;
-		}
+        // find solution of Ux = y
+        for (long i = n - 1; i >= 0; i--)
+        {
+            sum.setV(0);
+            for (long k = i + 1; k < n; k++) {
+            	lu.v(i, k, value1);
+            	x.v(k, value2);
+            	dbl.multiply(value1, value2, term);
+            	dbl.add(sum, term, sum);
+            }
+            dbl.unity(tmp);
+            lu.v(i, i, value1);
+            dbl.divide(tmp, value1, value1);
+            y.v(i, value2);
+            dbl.subtract(value2, sum, value2);
+            dbl.multiply(value1, value2, term);
+            x.setV(i, term);
+        }
 	}
 
-	// TODO: this code changes matrix a. Not ideal.
-	
-	private int decompose(Float64MatrixMember a, double tol, LinearStorage<?,SignedInt64Member> o, LinearStorage<?,Float64Member> s) {
-		SignedInt64Member v = new SignedInt64Member();
-		SignedInt64Member iIndex = new SignedInt64Member();
-		SignedInt64Member kIndex = new SignedInt64Member();
-		Float64Member tmp1 = dbl.construct();
-		Float64Member tmp2 = dbl.construct();
-		Float64Member factor = dbl.construct();
-		Float64Member entry = dbl.construct();
-		Float64Member aval = dbl.construct();
-		Float64Member sval = dbl.construct();
-
-		for (long i = 0; i < o.size(); i++) {
-			v.setV(i);
-			o.set(i,v);
-			a.v(i, 0, tmp1);
-			dbl.abs(tmp1, tmp1);
-			s.set(i, tmp1);
-			for (long j = 1; j < o.size(); j++) {
-				a.v(i, j, entry);
-				dbl.abs(entry,  entry);
-				s.get(i, tmp1);
-				if (dbl.isGreater(entry, tmp1)) {
-					s.set(i, entry);
-				}
-			}
-		}
-		for (long k = 0; k < o.size()-1; k++) {
-			pivot(a, o, s, k);
-			o.get(k, kIndex);
-			a.v(kIndex.v(), k, aval);
-			s.get(kIndex.v(), sval);
-			dbl.divide(aval, sval, tmp1);
-			if (Math.abs(tmp1.v()) < tol) {
-				System.out.println(tmp1.v());
-				return -1;
-			}
-			for (long i = k+1; i < o.size(); i++) {
-				o.get(i, iIndex);
-				a.v(iIndex.v(), k, tmp1);
-				a.v(kIndex.v(), k, tmp2);
-				dbl.divide(tmp1, tmp2, factor);
-				a.setV(iIndex.v(), k, factor);
-				for (long j = k+1; j < o.size(); j++) {
-					a.v(kIndex.v(), j, tmp1);
-					dbl.multiply(factor, tmp1, tmp1);
-					a.v(iIndex.v(), j, tmp2);
-					dbl.subtract(tmp2, tmp1, tmp2);
-					a.setV(iIndex.v(), j, tmp2);
-				}
-			}
-			a.v(kIndex.v(), k, tmp1);
-			s.get(kIndex.v(), tmp2);
-			dbl.divide(tmp1, tmp2, tmp1);
-			dbl.abs(tmp1, tmp1);
-			tmp2.setV(tol);
-			if (dbl.isLess(tmp1, tmp2)) {
-				System.out.println(tmp1);
-				return -1;
-			}
-		}
-
-		return 0;
-	}
-	
-	/*
-		for (int i = 0; i < n; i++) {
-			o[i] = i;
-			s[i] = Math.abs(a[i][0]);
-			for (j = 1; j < n; j++) {
-				double entry = Math.abs(a[i][j]);
-				if (entry > s[i])
-					s[i] = entry;
-			}
-		}
-		for (int k = 0; k < n-1; k++) {
-			pivot(a, o, s, n, k);
-			if (Math.abs(a[o[k]][k] / s[o[k]]) < tol) {
-				System.out.println(a[o[k]][k] / s[o[k]]);
-				return -1;
-			}
-			for (int i = k+1; i < n; i++) {
-				double factor = a[o[i]][k] / a[o[k]][k];
-				a[o[i]][k] = factor;
-				for (int j = k+1; j < n; j++) {
-					a[o[i]][j] -= factor * a[o[k]][j];
-				}
-			}
-		}
-		
-		if (Math.abs(a[o[k]][k]/s[o[k]]) < tol) {
-			System.out.println(a[o[k]][k] / s[o[k]]);
-			return -1;
-		}
-		return 0;
-
-	 */
-	
-	private void pivot(Float64MatrixMember a, LinearStorage<?,SignedInt64Member> o, LinearStorage<?,Float64Member> s, long k) {
-		SignedInt64Member kIndex = new SignedInt64Member();
-		SignedInt64Member qIndex = new SignedInt64Member();
-		Float64Member aval = new Float64Member();
-		Float64Member sval = new Float64Member();
-		Float64Member tmp1 = new Float64Member();
-		Float64Member max = new Float64Member();
-		Float64Member entry = new Float64Member();
-
-		long p = k;
-		o.get(k, kIndex);
-		a.v(kIndex.v(), k, aval);
-		s.get(kIndex.v(), sval);
-		dbl.divide(aval, sval, tmp1);
-		dbl.abs(tmp1, max);
-		for (long q=k+1; q < o.size(); q++) {
-			o.get(q, qIndex);
-			a.v(qIndex.v(), k, aval);
-			s.get(qIndex.v(), sval);
-			dbl.divide(aval, sval, tmp1);
-			dbl.abs(tmp1, entry);
-			if (dbl.isGreater(entry, max)) {
-				max.set(entry);
-				p = q;
-			}
-		}
-		if (p != k) {
-			o.get(p, qIndex);
-			o.get(k, kIndex);
-			o.set(p, kIndex);
-			o.set(k, qIndex);
-		}
-	}	
-	
-	/*
-	
-	private void pivot(double[][] a, int[] o, double[] s, int n, int k) {
-		int p = k;
-		double max = Math.abs(a[o[k]][k]/s[o[k]]);
-		for (int q = k+1; q < n; q++) {
-			double entry = Math.abs(a[o[q]][k]/s[o[q]]);
-			if (entry > max) {
-				max = entry;
-				p = q;
-			}
-		}
-		int tmp = o[p];
-		o[p] = o[k];
-		o[k] = tmp;
-	}
-
-	*/
-	
-	private void substitute(Float64MatrixMember a, LinearStorage<?,SignedInt64Member> o, Float64VectorMember b, Float64VectorMember x) {
-		Float64Member sum = new Float64Member();
-		Float64Member term = new Float64Member();
-		Float64Member aTerm = new Float64Member();
-		Float64Member bTerm = new Float64Member();
-		SignedInt64Member iIndex = new SignedInt64Member();
-		SignedInt64Member jIndex = new SignedInt64Member();
-		SignedInt64Member n1Index = new SignedInt64Member();
-		for (long i = 1; i < o.size(); i++) {
-			o.get(i, iIndex);
-			b.v(iIndex.v(), sum);
-			for (long j = 0; j < i-1; j++) {
-				a.v(iIndex.v(), j, aTerm);
-				o.get(j, jIndex);
-				b.v(jIndex.v(), bTerm);
-				dbl.multiply(aTerm, bTerm, term);
-				dbl.subtract(sum, term, sum);
-			}
-			b.setV(iIndex.v(), sum);
-		}
-		o.get(o.size()-1, n1Index);
-		b.v(n1Index.v(), bTerm);
-		a.v(n1Index.v(), o.size()-1, aTerm);
-		dbl.divide(bTerm, aTerm, term);
-		x.setV(o.size()-1, term);
-		for (long i = o.size()-2; i >= 0; i--) {
-			o.get(i, iIndex);
-			dbl.zero(sum);
-			for (long j = i+1; j < o.size(); j++) {
-				a.v(iIndex.v(), j, aTerm);
-				x.v(j, bTerm);
-				dbl.multiply(aTerm, bTerm, term);
-				dbl.add(sum,  term, sum);
-			}
-			a.v(iIndex.v(), i, aTerm);
-			b.v(iIndex.v(), bTerm);
-			dbl.subtract(bTerm, sum, bTerm);
-			dbl.divide(bTerm, aTerm, term);
-			x.setV(i, term);
-		}
-	}
-
-	/*
-	private void substitute(double[][] a, int[] o, int n, double[] b, double[] x) {
-		for (int i = 1; i < n; i++) {
-			double sum = b[o[i]];
-			for (int j = 0; j < i-1; j++) {
-				sum -= a[o[i]][j] * b[o[j]];
-			}
-			b[o[i]] = sum;
-		}
-		x[n-1] = b[o[n-1]]/a[o[n-1]][n-1];
-		for (int i = n-2; i >= 0; i--) {
-			double sum = 0;
-			for (int j = i+1; j < n; j++) {
-				sum += a[o[i]][j] * x[j];
-			}
-			x[i] = (b[o[i]] - sum) / a[o[i]][i];
-		}
-	}
-	*/
 }
