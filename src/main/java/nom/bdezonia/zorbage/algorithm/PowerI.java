2bb227efb91c07d44de37ab6f8943327f4d718c5
Barry DeZonia <bdezonia@gmail.com>
2017-12-07 22:39:43 -0600

    Minimize the number of multiplies in PowerI

--- src/main/java/nom/bdezonia/zorbage/algorithm/PowerI.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/PowerI.java
@@ -26,6 +26,7 @@
  */
 package nom.bdezonia.zorbage.algorithm;
 
+import nom.bdezonia.zorbage.type.algebra.Addition;
 import nom.bdezonia.zorbage.type.algebra.Group;
 import nom.bdezonia.zorbage.type.algebra.Multiplication;
 import nom.bdezonia.zorbage.type.algebra.Unity;
@@ -44,7 +45,7 @@ public class PowerI {
 	 * @param a
 	 * @param b
 	 */
-	public static <T extends Group<T,U> & Multiplication<U> & Unity<U>, U>
+	public static <T extends Group<T,U> & Addition<U> & Multiplication<U> & Unity<U>, U>
 		void compute(T group, int power, U a, U b)
 	{
 		if (power < 0)
@@ -52,12 +53,32 @@ public class PowerI {
 		U zero = group.construct();
 		if (power == 0 && group.isEqual(a,zero))
 			throw new IllegalArgumentException("0^0 is not a number");
-		// This is the nonrecursive slow implementation
-		U tmp = group.construct();
-		group.unity(tmp);
-		for (int i = 0; i < power; i++)
-			group.multiply(tmp, a, tmp);
-		group.assign(tmp, b);
+		// this code minimizes number of multiplies
+		U sum = group.construct();
+		U inc = group.construct();
+		group.unity(inc);
+		if (power == 0)
+			group.assign(inc, b);
+		// else power >= 1
+		int hiBit = hiBit(power);
+		U two = group.construct();
+		group.add(inc, inc, two);
+		for (int i = 0; i <= hiBit; i++) {
+			if ((power & i) > 0)
+				group.add(sum, inc, sum);
+			group.multiply(inc, two, inc);
+		}
+		group.assign(sum, b);
+	}
+	
+	private static int hiBit(int power) {
+		int tmp = 0x80000000;
+		for (int i = 31; i >= 0; i--) {
+			if ((power & tmp) > 0)
+				return i;
+			tmp >>>= 1;
+		}
+		throw new IllegalArgumentException("should not be possible to get here");
 	}
 
 }
