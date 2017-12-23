63a998795996a6c5fb8044ff78177eb15668538a
Barry DeZonia <bdezonia@gmail.com>
2017-12-22 18:59:46 -0600

    Add ParallelTransform as a proof of concept

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/ParallelTransform.java
@@ -0,0 +1,115 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2017 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.basic.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.storage.linear.LinearStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class ParallelTransform {
+
+	// TODO: this is a proof of concept. It should provide signatures for
+	// all of what Transform class can do. That may take more than one
+	// class.
+	
+	/**
+	 * 
+	 * @param grpU
+	 * @param grpW
+	 * @param a
+	 * @param b
+	 * @param proc
+	 * @param aStart
+	 * @param bStart
+	 * @param count
+	 */
+	public static <T extends Group<T,U>,U,V extends Group<V,W>,W>
+		void compute(T grpU, V grpW, LinearStorage<?,U> a, LinearStorage<?,W> b, Procedure2<U, W> proc, long aStart, long bStart, long count)
+	{
+		final int numProcs = Runtime.getRuntime().availableProcessors();
+		final Thread[] threads = new Thread[numProcs];
+		long thAStart = aStart;
+		long thBStart = bStart;
+		long thCount;
+		for (int i = 0; i < numProcs; i++) {
+			if (i == numProcs-1) {
+				thCount = (count / numProcs) + (count % numProcs);
+			}
+			else {
+				thCount = count / numProcs;
+			}
+			Runnable c = new Computer<T,U,V,W>(grpU, grpW, a, b, proc, thAStart, thBStart, thCount);
+			threads[i] = new Thread(c);
+			thAStart += thCount;
+			thBStart += thCount;
+		}
+		for (int i = 0; i < numProcs; i++) {
+			threads[i].start();
+		}
+		for (int i = 0; i < numProcs; i++) {
+			try {
+				threads[i].join();
+			} catch(InterruptedException e) {
+				throw new IllegalArgumentException("Thread execution error in ParallelTransform");
+			}
+		}
+	}
+	
+	private static class Computer<T extends Group<T,U>, U ,V extends Group<V,W>, W>
+		implements Runnable
+	{
+		private final T group1;
+		private final V group2;
+		private final LinearStorage<?,U> list1;
+		private final LinearStorage<?,W> list2;
+		private final Procedure2<U, W> proc;
+		private final long aStart;
+		private final long bStart;
+		private final long count;
+		
+		Computer(T grpU, V grpW, LinearStorage<?,U> a, LinearStorage<?,W> b, Procedure2<U, W> proc, long aStart, long bStart, long count) {
+			group1 = grpU;
+			group2 = grpW;
+			list1 = a;
+			list2 = b;
+			this.proc = proc;
+			this.aStart = aStart;
+			this.bStart = bStart;
+			this.count = count;
+		}
+		
+		public void run() {
+			Transform.compute(group1, group2, list1, list2, proc, aStart, bStart, count);
+		}
+	}
+}
+
---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestParallelTransform.java
@@ -0,0 +1,90 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2017 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Rand;
+import nom.bdezonia.zorbage.procedure.Sin;
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.algebra.Random;
+import nom.bdezonia.zorbage.type.algebra.Trigonometric;
+import nom.bdezonia.zorbage.type.storage.linear.LinearStorage;
+import nom.bdezonia.zorbage.type.storage.linear.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestParallelTransform {
+
+	@Test
+	public void testFloats()
+	{
+		test(G.DBL);
+	}
+
+	@Test
+	public void testComplexes() {
+		test(G.CDBL);
+	}
+	
+	@Test
+	public void testQuats() {
+		test(G.QDBL);
+	}
+	
+	@Test
+	public void testOcts() {
+		test(G.ODBL);
+	}
+
+	// an algorithm that applies Sin() op to a list of any type that
+	// supports sin()
+	
+	private <T extends Group<T,U> & Trigonometric<U> & Random<U>, U>
+		void test(T group)
+	{
+		// generic allocation
+		LinearStorage<?,U> a = ArrayStorage.allocate(100, group.construct());
+		
+		// set values of storage to random doubles between 0 and 1
+		Rand<T, U> randOp = new Rand<T,U>(group);
+		// TODO: don't do parallel xform until Random is thread friendly
+		Transform.compute(group, a, randOp);
+		
+		// transform each input[i] value to be the sin(input[i])
+		Sin<T, U> sinOp = new Sin<T,U>(group);
+		ParallelTransform.compute(group, group, a, a, sinOp, 0, 0, a.size());
+		
+		assertTrue(true);
+	}
+}
