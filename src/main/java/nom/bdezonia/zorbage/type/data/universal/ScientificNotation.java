e00eab4ae40925484d2346c39d73a736132260e6
Barry DeZonia <bdezonia@gmail.com>
2018-01-13 22:05:33 -0600

    Define a way to construct real values via scientific notation

---
+++ src/main/java/nom/bdezonia/zorbage/type/data/universal/ScientificNotation.java
@@ -0,0 +1,60 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.data.universal;
+
+import java.math.BigDecimal;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class ScientificNotation {
+
+	/**
+	 * Create a numeric value from an scientific notation description.
+	 * Works in BigDecimals for accuracy. Can only create Real values.
+	 * Result must support UniversalRepresentation.
+	 * 
+	 * @param fraction
+	 * @param base
+	 * @param power
+	 * @param result
+	 */
+	public static <U extends UniversalRepresentation>
+		void compute(BigDecimal fraction, int base, int power, U result)
+	{
+		BigDecimal b = BigDecimal.valueOf(base);
+		BigDecimal tmp = b.pow(power);
+		BigDecimal value = tmp.multiply(fraction);
+		OctonionRepresentation v = new OctonionRepresentation();
+		v.setR(value);
+		TensorOctonionRepresentation rep = new TensorOctonionRepresentation();
+		rep.setFirstValue(v);
+		result.setSelfFromTensor(rep);
+	}
+}
