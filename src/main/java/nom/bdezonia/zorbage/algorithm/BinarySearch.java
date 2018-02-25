68b57d3178c16748a5fc20fec24bed189d454e8d
Barry DeZonia <bdezonia@gmail.com>
2018-02-24 21:25:30 -0600

    Add a binary search algorithm

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/BinarySearch.java
@@ -0,0 +1,75 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.algebra.Ordered;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class BinarySearch {
+
+	private BinarySearch() {
+		// do not instantiate
+	}
+	
+	/**
+	 * Search a list that is sorted in ascending order for a given value.
+	 * Return the index within the list where the value is found. Return
+	 * -1 if the element is not found.
+	 * 
+	 * @param group
+	 * @param data
+	 * @param start
+	 * @param count
+	 * @param value
+	 * @return
+	 */
+	public static <T extends Group<T,U> & Ordered<U>, U>
+		long compute(T group, IndexedDataSource<?,U> data, long start, long count, U value)
+	{
+		U tmp = group.construct();
+		long lo = 0;
+		long hi = count-1;
+		while (hi >= lo) {
+			long mid = (lo + hi) / 2;
+			data.get(start+mid, tmp);
+			int cmp = group.compare(tmp, value);
+			if (cmp == 0)
+				return start + mid;
+			else if (cmp < 0)
+				lo = mid + 1;
+			else // cmp > 0
+				hi = mid - 1;
+		}
+		return -1;
+	}
+}
