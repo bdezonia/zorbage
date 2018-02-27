14d522fdc5466491fa058e0e505b1b0bf8187d64
Barry DeZonia <bdezonia@gmail.com>
2018-02-26 21:42:39 -0600

    Test determinant code against known data

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestLUDecomp.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestLUDecomp.java
@@ -269,4 +269,59 @@ public class TestLUDecomp {
 		
 		assertEquals(-2, val.v(), 0);
 	}
+
+	// In wolfram alpha type:
+	//   "LU decomposition of {{4,3,2,1},{1,10,3,4},{5,3,2,-4},{4,8,7,9}}"
+	// Then multiply the diagonal to get 602.
+	
+	@Test
+	public void testReal4x4Det() {
+		Float64Member val = G.DBL.construct();
+
+		Float64MatrixMember a = G.DBL_MAT.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, 4, 4);
+		
+		val.setV(4);
+		a.setV(0, 0, val);
+		val.setV(3);
+		a.setV(0, 1, val);
+		val.setV(2);
+		a.setV(0, 2, val);
+		val.setV(1);
+		a.setV(0, 3, val);
+
+		val.setV(1);
+		a.setV(1, 0, val);
+		val.setV(10);
+		a.setV(1, 1, val);
+		val.setV(3);
+		a.setV(1, 2, val);
+		val.setV(4);
+		a.setV(1, 3, val);
+
+		val.setV(5);
+		a.setV(2, 0, val);
+		val.setV(3);
+		a.setV(2, 1, val);
+		val.setV(2);
+		a.setV(2, 2, val);
+		val.setV(-4);
+		a.setV(2, 3, val);
+
+		val.setV(4);
+		a.setV(3, 0, val);
+		val.setV(8);
+		a.setV(3, 1, val);
+		val.setV(7);
+		a.setV(3, 2, val);
+		val.setV(9);
+		a.setV(3, 3, val);
+		
+		Float64VectorMember b = G.DBL_VEC.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, 4);
+		
+		Float64VectorMember x = G.DBL_VEC.construct(MemoryConstruction.DENSE, StorageConstruction.ARRAY, 4);
+
+		LUDecomp.compute(G.DBL, G.DBL_VEC, G.DBL_MAT, a, b, x, val);
+		
+		assertEquals(602, val.v(), 0);
+	}
 }
