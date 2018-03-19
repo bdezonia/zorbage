2aae5e9b08ac6fd7c273eb490aa9232762a5ee2c
Barry DeZonia <bdezonia@gmail.com>
2018-03-18 20:38:54 -0500

    Define Convolve, the complement to Correlate

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Convolve.java
@@ -0,0 +1,81 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.algebra.Multiplication;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Convolve {
+
+	// do not instantiate
+	
+	private Convolve() {}
+
+	/**
+	 * 
+	 * @param group
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
+	public static <T extends Group<T,U> & Multiplication<U>,U>
+		void compute(T group, IndexedDataSource<?,U> a, IndexedDataSource<?,U> b, IndexedDataSource<?,U> c)
+	{
+		U tmpA1 = group.construct();
+		U tmpA2 = group.construct();
+		U tmpB1 = group.construct();
+		U tmpB2 = group.construct();
+		U tmpC1 = group.construct();
+		U tmpC2 = group.construct();
+
+		if (a.size() != b.size())
+			throw new IllegalArgumentException("mismatched inputs");
+		if (a.size() != c.size())
+			throw new IllegalArgumentException("mismatched input/output");
+		
+		for (long i = 0, j = a.size()-1; i <= j; i++,j--) {
+			// Note:
+			// The order of these operations is designed so that
+			// c can possibly be a or b or some other list and the
+			// convolution will not break.
+			a.get(i, tmpA1);
+			b.get(j, tmpB1);
+			group.multiply(tmpA1, tmpB1, tmpC1);
+			a.get(j, tmpA2);
+			b.get(i, tmpB2);
+			group.multiply(tmpA2, tmpB2, tmpC2);
+			c.set(i, tmpC1);
+			c.set(j, tmpC2);
+		}
+	}
+}
