a250383463821cf0f8b4c3aa3b3287b164247ef9
Barry DeZonia <bdezonia@gmail.com>
2018-03-25 12:18:47 -0500

    Define some bridge classes from matrixes/rmodules to numbers

---
+++ src/main/java/nom/bdezonia/zorbage/type/data/helper/MatrixElementNumberBridge.java
@@ -0,0 +1,77 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.data.helper;
+
+import nom.bdezonia.zorbage.type.algebra.MatrixMember;
+import nom.bdezonia.zorbage.type.algebra.NumberMember;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class MatrixElementNumberBridge<U> implements NumberMember<U> {
+
+	private final MatrixMember<U> mat;
+	private long row;
+	private long col;
+	
+	public MatrixElementNumberBridge(MatrixMember<U> mat) {
+		this.mat = mat;
+		this.row = 0;
+		this.col = 0;
+	}
+
+	public void setRowCol(long r, long c) {
+		if (r < 0 || r >= mat.rows() || c < 0 || c >= mat.cols())
+			throw new IllegalArgumentException("number bridge set outside matrix bounds");
+		row = r;
+		col = c;
+	}
+	
+	@Override
+	public long dimension(int d) {
+		if (d < 0)
+			throw new IllegalArgumentException("negative dimension error");
+		return 1;
+	}
+
+	@Override
+	public int numDimensions() {
+		return 0;
+	}
+
+	@Override
+	public void v(U value) {
+		mat.v(row, col, value);
+	}
+
+	@Override
+	public void setV(U value) {
+		mat.setV(row, col, value);
+	}
+}
