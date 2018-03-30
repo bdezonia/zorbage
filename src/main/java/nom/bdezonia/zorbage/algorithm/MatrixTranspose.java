1bd14f8128c8f510fcf16a6ba06874fcdbbd825a
Barry DeZonia <bdezonia@gmail.com>
2018-03-29 19:36:52 -0500

    Make MatrixTranspose algorithm a little more clear

--- src/main/java/nom/bdezonia/zorbage/algorithm/MatrixTranspose.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/MatrixTranspose.java
@@ -43,28 +43,28 @@ public class MatrixTranspose {
 	public static <T extends Group<T,U>,U>
 		void compute(T group, MatrixMember<U> a, MatrixMember<U> b)
 	{
-		U value = group.construct();
+		U value1 = group.construct();
 		U value2 = group.construct();
 		if (a == b) {
-			// can transpose square mats in place
-			if (a.rows() == a.cols()) {
-				for (long d = 1; d < a.rows(); d++) {
-					for (long c = 1; c <= d; c++) {
-						a.v(d-c, d, value);
-						a.v(d, d-c, value2);
-						a.setV(d-c, d, value2);
-						a.setV(d, d-c, value);
-					}
+			if (a.rows() != a.cols())
+				throw new IllegalArgumentException("in place transpose only works for square matrices");
+			// else can transpose square mats in place
+			for (long d = 1; d < a.rows(); d++) {
+				for (long c = 1; c <= d; c++) {
+					a.v(d-c, d, value1);
+					a.v(d, d-c, value2);
+					a.setV(d-c, d, value2);
+					a.setV(d, d-c, value1);
 				}
-				return;
 			}
-			throw new IllegalArgumentException("in place transpose only works for square matrices");
 		}
-		b.alloc(a.cols(), a.rows());
-		for (long r = 0; r < a.rows(); r++) {
-			for (long c = 0; c < a.cols(); c++) {
-				a.v(r,  c, value);
-				b.setV(c, r, value);
+		else { // the output b is different from the input a
+			b.alloc(a.cols(), a.rows());
+			for (long r = 0; r < a.rows(); r++) {
+				for (long c = 0; c < a.cols(); c++) {
+					a.v(r,  c, value1);
+					b.setV(c, r, value1);
+				}
 			}
 		}
 	}
