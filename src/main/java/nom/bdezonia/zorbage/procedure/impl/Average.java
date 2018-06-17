e804592906aeeac9b07ecec5e0c791d9cff3145f
Barry DeZonia <bdezonia@gmail.com>
2018-06-16 21:41:31 -0500

    Add some generic procedures

---
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/Average.java
@@ -0,0 +1,68 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.procedure.impl;
+
+import nom.bdezonia.zorbage.procedure.Procedure;
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+import nom.bdezonia.zorbage.type.algebra.Unity;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Average<T extends Group<T,U> & Unity<U> & Addition<U> & Invertible<U>,U>
+	implements Procedure<U>
+{
+	private final T group;
+	
+	public Average(T group) {
+		this.group = group;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void call(U result, U... inputs) {
+		if (inputs.length == 0) {
+			group.zero(result);
+		}
+		else {
+			U count = group.construct();
+			U sum = group.construct();
+			U one = group.construct();
+			group.unity(one);
+			for (int i = 0; i < inputs.length; i++) {
+				group.add(count, one, count);
+				group.add(sum, inputs[i], sum);
+			}
+			group.divide(count, sum, result);
+		}
+	}
+
+}
