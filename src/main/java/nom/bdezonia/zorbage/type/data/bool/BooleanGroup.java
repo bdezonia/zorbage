3838351ef9bd4757ff2ba9772fda6bdfd032d112
Barry DeZonia <bdezonia@gmail.com>
2018-07-15 00:01:37 -0500

    Further refactoring: BooleanGroup

--- src/main/java/nom/bdezonia/zorbage/type/data/bool/BooleanGroup.java
+++ src/main/java/nom/bdezonia/zorbage/type/data/bool/BooleanGroup.java
@@ -30,6 +30,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import nom.bdezonia.zorbage.algorithm.Max;
 import nom.bdezonia.zorbage.algorithm.Min;
+import nom.bdezonia.zorbage.function.Function1;
+import nom.bdezonia.zorbage.function.Function2;
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Procedure1;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.procedure.Procedure3;
+import nom.bdezonia.zorbage.procedure.Procedure4;
 import nom.bdezonia.zorbage.type.algebra.Bounded;
 import nom.bdezonia.zorbage.type.algebra.CommutativeRingWithUnity;
 import nom.bdezonia.zorbage.type.algebra.LogicalOperations;
@@ -58,83 +65,219 @@ public class BooleanGroup
 	
 	public BooleanGroup() {
 	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> AND =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			c.setV(a.v() && b.v());
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> logicalAnd() {
+		return AND;
+	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> OR =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			c.setV(a.v() || b.v());
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> logicalOr() {
+		return OR;
+	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> XOR =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			c.setV(a.v() ^ b.v());
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> logicalXor() {
+		return XOR;
+	}
+
+	private final Procedure2<BooleanMember, BooleanMember> NOT =
+			new Procedure2<BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b) {
+			b.setV(!a.v());
+		}
+	};
+
+	@Override
+	public Procedure2<BooleanMember, BooleanMember> logicalNot() {
+		return NOT;
+	}
 	
-	@Override
-	public void logicalAnd(BooleanMember a, BooleanMember b, BooleanMember c) {
-		c.setV(a.v() && b.v());
-	}
+	private final Procedure4<BooleanMember, BooleanMember, BooleanMember, BooleanMember> TERN =
+			new Procedure4<BooleanMember, BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c, BooleanMember d) {
+			d.setV(a.v() ? b.v() : c.v());
+		}
+	};
 
-	@Override
-	public void logicalOr(BooleanMember a, BooleanMember b, BooleanMember c) {
-		c.setV(a.v() || b.v());
-	}
-
-	@Override
-	public void logicalXor(BooleanMember a, BooleanMember b, BooleanMember c) {
-		c.setV(a.v() ^ b.v());
-	}
-
-	@Override
-	public void logicalNot(BooleanMember a, BooleanMember b) {
-		b.setV(!a.v());
-	}
-	
 	// not an override
-	public void ternary(BooleanMember a, BooleanMember b, BooleanMember c, BooleanMember d) {
-		d.setV(a.v() ? b.v() : c.v());
+	public Procedure4<BooleanMember, BooleanMember, BooleanMember, BooleanMember> ternary() {
+		return TERN;
 	}
 
+	private final Function2<Boolean,BooleanMember,BooleanMember> LS =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return !a.v() && b.v();
+		}
+	};
+			
 	@Override
-	public boolean isLess(BooleanMember a, BooleanMember b) {
-		return !a.v() && b.v();
+	public Function2<Boolean,BooleanMember,BooleanMember> isLess() {
+		return LS;
 	}
 
+	private final Function2<Boolean,BooleanMember,BooleanMember> LSE =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return !a.v();
+		}
+	};
+			
 	@Override
-	public boolean isLessEqual(BooleanMember a, BooleanMember b) {
-		return !a.v();
+	public Function2<Boolean,BooleanMember,BooleanMember> isLessEqual() {
+		return LSE;
 	}
 
+
+	private final Function2<Boolean,BooleanMember,BooleanMember> GR =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return a.v() && !b.v();
+		}
+	};
+			
 	@Override
-	public boolean isGreater(BooleanMember a, BooleanMember b) {
-		return a.v() && !b.v();
+	public Function2<Boolean,BooleanMember,BooleanMember> isGreater() {
+		return GR;
 	}
 
+	private final Function2<Boolean,BooleanMember,BooleanMember> GRE =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return a.v();
+		}
+	};
+			
 	@Override
-	public boolean isGreaterEqual(BooleanMember a, BooleanMember b) {
-		return a.v();
+	public Function2<Boolean,BooleanMember,BooleanMember> isGreaterEqual() {
+		return GRE;
 	}
 
+	private final Function2<java.lang.Integer,BooleanMember,BooleanMember> CMP =
+			new Function2<Integer, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Integer call(BooleanMember a, BooleanMember b) {
+			if (isLess().call(a,b)) return -1;
+			if (isGreater().call(a,b)) return 1;
+			return 0;
+		}
+	};
+	
 	@Override
-	public int compare(BooleanMember a, BooleanMember b) {
-		if (isLess(a,b)) return -1;
-		if (isGreater(a,b)) return 1;
-		return 0;
+	public Function2<java.lang.Integer,BooleanMember,BooleanMember> compare() {
+		return CMP;
 	}
 
+	private final Function1<java.lang.Integer,BooleanMember> SIG =
+			new Function1<Integer, BooleanMember>()
+	{
+		@Override
+		public Integer call(BooleanMember a) {
+			if (a.v()) return 1;
+			return 0;
+		}
+	};
+	
 	@Override
-	public int signum(BooleanMember a) {
-		if (a.v()) return 1;
-		return 0;
+	public Function1<java.lang.Integer,BooleanMember> signum() {
+		return SIG;
 	}
 
-	@Override
-	public void maxBound(BooleanMember a) {
-		a.setV(true);
-	}
+	private final Procedure1<BooleanMember> MAXB =
+			new Procedure1<BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a) {
+			a.setV(true);
+		}
+	};
 
 	@Override
-	public void minBound(BooleanMember a) {
-		a.setV(false);
+	public Procedure1<BooleanMember> maxBound() {
+		return MAXB;
 	}
 
-	@Override
-	public boolean isEqual(BooleanMember a, BooleanMember b) {
-		return a.v() == b.v();
-	}
+	private final Procedure1<BooleanMember> MINB =
+			new Procedure1<BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a) {
+			a.setV(false);
+		}
+	};
 
 	@Override
-	public boolean isNotEqual(BooleanMember a, BooleanMember b) {
-		return a.v() != b.v();
+	public Procedure1<BooleanMember> minBound() {
+		return MINB;
+	}
+
+	private final Function2<Boolean,BooleanMember,BooleanMember> EQ =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return a.v() == b.v();
+		}
+	};
+
+	@Override
+	public Function2<Boolean,BooleanMember,BooleanMember> isEqual() {
+		return EQ;
+	}
+
+	private final Function2<Boolean,BooleanMember,BooleanMember> NEQ =
+			new Function2<Boolean, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public Boolean call(BooleanMember a, BooleanMember b) {
+			return a.v() != b.v();
+		}
+	};
+
+	@Override
+	public Function2<Boolean,BooleanMember,BooleanMember> isNotEqual() {
+		return NEQ;
 	}
 
 	@Override
@@ -152,46 +295,109 @@ public class BooleanGroup
 		return new BooleanMember(s);
 	}
 
-	@Override
-	public void assign(BooleanMember from, BooleanMember to) {
-		to.setV(from.v());
-	}
-
-	@Override
-	public void min(BooleanMember a, BooleanMember b, BooleanMember c) {
-		Min.compute(this, a, b, c);
-	}
-
-	@Override
-	public void max(BooleanMember a, BooleanMember b, BooleanMember c) {
-		Max.compute(this, a, b, c);
-	}
-
-	@Override
-	public void multiply(BooleanMember a, BooleanMember b, BooleanMember c) {
-		c.setV(a.v() && b.v());
-	}
-
-	@Override
-	public void power(int power, BooleanMember a, BooleanMember b) {
-		if (power < 0)
-			throw new IllegalArgumentException("booleans cannot handle negative powers");
-		if (power == 0) {
-			if (a.v() == false) throw new IllegalArgumentException("0^0 is not a number");
-			b.setV(true);
+	private final Procedure2<BooleanMember,BooleanMember> ASSIGN =
+			new Procedure2<BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember from, BooleanMember to) {
+			to.setV(from.v());
 		}
-		else
-			b.setV(a.v());
-	}
+	};
 
 	@Override
-	public void zero(BooleanMember a) {
-		a.setV(false);
+	public Procedure2<BooleanMember,BooleanMember> assign() {
+		return ASSIGN;
 	}
 
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> MIN =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			Min.compute(G.BOOL, a, b, c);
+		}
+	};
+
 	@Override
-	public void negate(BooleanMember a, BooleanMember b) {
-		b.setV(!a.v());
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> min() {
+		return MIN;
+	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> MAX =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			Max.compute(G.BOOL, a, b, c);
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> max() {
+		return MAX;
+	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> MUL =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			c.setV(a.v() && b.v());
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> multiply() {
+		return MUL;
+	}
+
+	private final Procedure3<java.lang.Integer, BooleanMember, BooleanMember> POWER =
+			new Procedure3<java.lang.Integer, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(java.lang.Integer power, BooleanMember a, BooleanMember b) {
+			if (power < 0)
+				throw new IllegalArgumentException("booleans cannot handle negative powers");
+			if (power == 0) {
+				if (a.v() == false) throw new IllegalArgumentException("0^0 is not a number");
+				b.setV(true);
+			}
+			else
+				b.setV(a.v());
+		}
+	};
+
+	@Override
+	public Procedure3<java.lang.Integer, BooleanMember, BooleanMember> power() {
+		return POWER;
+	}
+
+	private final Procedure1<BooleanMember> ZERO =
+			new Procedure1<BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a) {
+			a.setV(false);
+		}
+	};
+	
+	@Override
+	public Procedure1<BooleanMember> zero() {
+		return ZERO;
+	}
+
+	private final Procedure2<BooleanMember,BooleanMember> NEG =
+			new Procedure2<BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b) {
+			b.setV(!a.v());
+		}
+	};
+
+	@Override
+	public Procedure2<BooleanMember,BooleanMember> negate() {
+		return NEG;
 	}
 
 	// overflow possible: not sure if I like the placement of this class in the algebra hierarchy.
@@ -199,13 +405,22 @@ public class BooleanGroup
 	//   Also note if we support the overflowers then the type should add pred() and succ(). And
 	//   maybe also the BitOperations
 	
-	@Override
-	public void add(BooleanMember a, BooleanMember b, BooleanMember c) {
-		if (a.v()) {
-			c.setV(!b.v()); // includes overflow case
-		} else { // a.v == false
-			c.setV(b.v());
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> ADD =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			if (a.v()) {
+				c.setV(!b.v()); // includes overflow case
+			} else { // a.v == false
+				c.setV(b.v());
+			}
 		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> add() {
+		return ADD;
 	}
 
 	// underflow possible: not sure if I like the placement of this class in the algebra hierarchy.
@@ -213,30 +428,66 @@ public class BooleanGroup
 	//   Also note if we support the overflowers then the type should add pred() and succ(). And
 	//   maybe also the BitOperations
 	
-	@Override
-	public void subtract(BooleanMember a, BooleanMember b, BooleanMember c) {
-		if (a.v()) {
-			c.setV(!b.v());
-		} else { // a.v == false
-			c.setV(b.v()); // includes underflow case
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> SUB =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			if (a.v()) {
+				c.setV(!b.v());
+			} else { // a.v == false
+				c.setV(b.v()); // includes underflow case
+			}
 		}
-	}
+	};
 
 	@Override
-	public void unity(BooleanMember a) {
-		a.setV(true);
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> subtract() {
+		return SUB;
 	}
 
-	@Override
-	public void random(BooleanMember a) {
-		ThreadLocalRandom rng = ThreadLocalRandom.current();
-		a.setV(rng.nextBoolean());
-	}
+	private final Procedure1<BooleanMember> ONE =
+			new Procedure1<BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a) {
+			a.setV(true);
+		}
+	};
 
 	@Override
-	public void pow(BooleanMember a, BooleanMember b, BooleanMember c) {
-		int p = b.v() ? 1 : 0;
-		power(p, a, c);
+	public Procedure1<BooleanMember> unity() {
+		return ONE;
+	}
+
+	private final Procedure1<BooleanMember> RAND =
+			new Procedure1<BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a) {
+			ThreadLocalRandom rng = ThreadLocalRandom.current();
+			a.setV(rng.nextBoolean());
+		}
+	};
+
+	@Override
+	public Procedure1<BooleanMember> random() {
+		return RAND;
+	}
+
+	private final Procedure3<BooleanMember, BooleanMember, BooleanMember> POW =
+			new Procedure3<BooleanMember, BooleanMember, BooleanMember>()
+	{
+		@Override
+		public void call(BooleanMember a, BooleanMember b, BooleanMember c) {
+			int p = b.v() ? 1 : 0;
+			power().call(p, a, c);
+		}
+	};
+
+	@Override
+	public Procedure3<BooleanMember, BooleanMember, BooleanMember> pow() {
+		return POW;
 	}
 
 }
