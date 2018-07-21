9133f92e614c0f85ae96343fc04a64f5b8c3b280
Barry DeZonia <bdezonia@gmail.com>
2018-07-21 02:26:53 -0500

    Further refactoring: quaternion float64 rmodule group

--- src/main/java/nom/bdezonia/zorbage/type/data/float64/quaternion/QuaternionFloat64RModule.java
+++ src/main/java/nom/bdezonia/zorbage/type/data/float64/quaternion/QuaternionFloat64RModule.java
@@ -37,7 +37,12 @@ import nom.bdezonia.zorbage.algorithm.RModuleIsEqual;
 import nom.bdezonia.zorbage.algorithm.RModuleNegate;
 import nom.bdezonia.zorbage.algorithm.RModuleScale;
 import nom.bdezonia.zorbage.algorithm.RModuleSubtract;
+import nom.bdezonia.zorbage.function.Function2;
 import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Procedure1;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.procedure.Procedure3;
+import nom.bdezonia.zorbage.procedure.Procedure4;
 import nom.bdezonia.zorbage.type.algebra.DirectProduct;
 import nom.bdezonia.zorbage.type.algebra.Norm;
 import nom.bdezonia.zorbage.type.algebra.Products;
@@ -63,35 +68,94 @@ public class QuaternionFloat64RModule
 	
 	public QuaternionFloat64RModule() { }
 	
+	private final Procedure1<QuaternionFloat64RModuleMember> ZER = 
+			new Procedure1<QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a) {
+			for (long i = 0; i < a.length(); i++)
+				a.setV(i, ZERO);
+		}
+	};
+	
 	@Override
-	public void zero(QuaternionFloat64RModuleMember a) {
-		for (long i = 0; i < a.length(); i++)
-			a.setV(i, ZERO);
+	public Procedure1<QuaternionFloat64RModuleMember> zero() {
+		return ZER;
 	}
+	
+	private final Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> NEG =
+			new Procedure2<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
+			RModuleNegate.compute(G.QDBL, a, b);
+		}
+	};
+	
 
 	@Override
-	public void negate(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
-		RModuleNegate.compute(G.QDBL, a, b);
+	public Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> negate() {
+		return NEG;
 	}
 
-	@Override
-	public void add(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64RModuleMember c) {
-		RModuleAdd.compute(G.QDBL, a, b, c);
-	}
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> ADD =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b,
+				QuaternionFloat64RModuleMember c)
+		{
+			RModuleAdd.compute(G.QDBL, a, b, c);
+		}
+	};
 
 	@Override
-	public void subtract(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64RModuleMember c) {
-		RModuleSubtract.compute(G.QDBL, a, b, c);
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> add() {
+		return ADD;
 	}
 
-	@Override
-	public boolean isEqual(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
-		return RModuleIsEqual.compute(G.QDBL, a, b);
-	}
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> SUB =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b,
+				QuaternionFloat64RModuleMember c)
+		{
+			RModuleSubtract.compute(G.QDBL, a, b, c);
+		}
+	};
 
 	@Override
-	public boolean isNotEqual(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
-		return !isEqual(a,b);
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> subtract() {
+		return SUB;
+	}
+
+	private Function2<Boolean,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> EQ =
+			new Function2<Boolean, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public Boolean call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
+			return RModuleIsEqual.compute(G.QDBL, a, b);
+		}
+	};
+	
+	@Override
+	public Function2<Boolean,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> isEqual() {
+		return EQ;
+	}
+
+	private final Function2<Boolean,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> NEQ =
+			new Function2<Boolean, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public Boolean call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
+			return !isEqual().call(a,b);
+		}
+	};
+	
+	@Override
+	public Function2<Boolean,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> isNotEqual() {
+		return NEQ;
 	}
 
 	@Override
@@ -114,79 +178,185 @@ public class QuaternionFloat64RModule
 		return new QuaternionFloat64RModuleMember(s, d1);
 	}
 
-	@Override
-	public void assign(QuaternionFloat64RModuleMember from, QuaternionFloat64RModuleMember to) {
-		RModuleAssign.compute(G.QDBL, from, to);
-	}
-
-	@Override
-	public void norm(QuaternionFloat64RModuleMember a, Float64Member b) {
-		QuaternionFloat64Member aTmp = new QuaternionFloat64Member();
-		QuaternionFloat64Member sum = new QuaternionFloat64Member();
-		QuaternionFloat64Member tmp = new QuaternionFloat64Member();
-		// TODO Look into preventing overflow. can do so similar to float case using norms
-		for (long i = 0; i < a.length(); i++) {
-			a.v(i, aTmp);
-			G.QDBL.conjugate(aTmp, tmp);
-			G.QDBL.multiply(aTmp, tmp, tmp);
-			G.QDBL.add(sum, tmp, sum);
+	private final Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> ASSIGN = 
+			new Procedure2<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember from, QuaternionFloat64RModuleMember to) {
+			RModuleAssign.compute(G.QDBL, from, to);
 		}
-		b.setV(Math.sqrt(sum.r()));
+	};
+	
+	@Override
+	public Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> assign() {
+		return ASSIGN;
 	}
 
-	@Override
-	public void scale(QuaternionFloat64Member scalar, QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
-		RModuleScale.compute(G.QDBL, scalar, a, b);
-	}
-
-	@Override
-	public void crossProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64RModuleMember c) {
-		CrossProduct.compute(G.QDBL_MOD, G.QDBL, a, b, c);
-	}
-
-	@Override
-	public void dotProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64Member c) {
-		DotProduct.compute(G.QDBL, a, b, c);
-	}
-
-	@Override
-	public void perpDotProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64Member c) {
-		PerpDotProduct.compute(G.QDBL_MOD, G.QDBL, a, b, c);
-	}
-
-	@Override
-	public void vectorTripleProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64RModuleMember c,
-			QuaternionFloat64RModuleMember d)
+	private final Procedure2<QuaternionFloat64RModuleMember,Float64Member> NORM =
+			new Procedure2<QuaternionFloat64RModuleMember, Float64Member>()
 	{
-		QuaternionFloat64RModuleMember b_cross_c = new QuaternionFloat64RModuleMember(new double[3*4]);
-		crossProduct(b, c, b_cross_c);
-		crossProduct(a, b_cross_c, d);
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, Float64Member b) {
+			QuaternionFloat64Member aTmp = new QuaternionFloat64Member();
+			QuaternionFloat64Member sum = new QuaternionFloat64Member();
+			QuaternionFloat64Member tmp = new QuaternionFloat64Member();
+			// TODO Look into preventing overflow. can do so similar to float case using norms
+			for (long i = 0; i < a.length(); i++) {
+				a.v(i, aTmp);
+				G.QDBL.conjugate().call(aTmp, tmp);
+				G.QDBL.multiply().call(aTmp, tmp, tmp);
+				G.QDBL.add().call(sum, tmp, sum);
+			}
+			b.setV(Math.sqrt(sum.r()));
+		}
+	};
+	
+	@Override
+	public Procedure2<QuaternionFloat64RModuleMember,Float64Member> norm() {
+		return NORM;
 	}
 
-	@Override
-	public void scalarTripleProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64RModuleMember c,
-			QuaternionFloat64Member d)
+	private final Procedure3<QuaternionFloat64Member,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> SCALE =
+			new Procedure3<QuaternionFloat64Member, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
 	{
-		QuaternionFloat64RModuleMember b_cross_c = new QuaternionFloat64RModuleMember(new double[3*4]);
-		crossProduct(b, c, b_cross_c);
-		dotProduct(a, b_cross_c, d);
+		@Override
+		public void call(QuaternionFloat64Member scalar, QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
+			RModuleScale.compute(G.QDBL, scalar, a, b);
+		}
+	};
+	
+	@Override
+	public Procedure3<QuaternionFloat64Member,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> scale() {
+		return SCALE;
 	}
 
-	@Override
-	public void conjugate(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
-		RModuleConjugate.compute(G.QDBL, a, b);
-	}
-
-	@Override
-	public void vectorDirectProduct(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64MatrixMember c) {
-		directProduct(a, b, c);
-	}
-
-	@Override
-	public void directProduct(QuaternionFloat64RModuleMember in1, QuaternionFloat64RModuleMember in2,
-			QuaternionFloat64MatrixMember out)
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> CROSS =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
 	{
-		RModuleDirectProduct.compute(G.QDBL, in1, in2, out);
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b,
+				QuaternionFloat64RModuleMember c)
+		{
+			CrossProduct.compute(G.QDBL_MOD, G.QDBL, a, b, c);
+		}
+	};
+
+	@Override
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> crossProduct() {
+		return CROSS;
+	}
+
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> DOT =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64Member>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64Member c) {
+			DotProduct.compute(G.QDBL, a, b, c);
+		}
+	};
+	
+	@Override
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> dotProduct() {
+		return DOT;
+	}
+
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> PERP =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64Member>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b, QuaternionFloat64Member c) {
+			PerpDotProduct.compute(G.QDBL_MOD, G.QDBL, a, b, c);
+		}
+	};
+	
+	@Override
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> perpDotProduct() {
+		return PERP;
+	}
+
+	private final Procedure4<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> VTRIPLE =
+			new Procedure4<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b,
+				QuaternionFloat64RModuleMember c, QuaternionFloat64RModuleMember d)
+		{
+			QuaternionFloat64RModuleMember b_cross_c = new QuaternionFloat64RModuleMember(new double[3*4]);
+			crossProduct().call(b, c, b_cross_c);
+			crossProduct().call(a, b_cross_c, d);
+		}
+	};
+	
+	@Override
+	public Procedure4<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> vectorTripleProduct()
+	{
+		return VTRIPLE;
+	}
+
+	private final Procedure4<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> STRIPLE =
+			new Procedure4<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64Member>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b,
+				QuaternionFloat64RModuleMember c, QuaternionFloat64Member d)
+		{
+			QuaternionFloat64RModuleMember b_cross_c = new QuaternionFloat64RModuleMember(new double[3*4]);
+			crossProduct().call(b, c, b_cross_c);
+			dotProduct().call(a, b_cross_c, d);
+		}
+	};
+	
+	@Override
+	public Procedure4<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64Member> scalarTripleProduct()
+	{
+		return STRIPLE;
+	}
+
+	private final Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> CONJ =
+			new Procedure2<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember a, QuaternionFloat64RModuleMember b) {
+			RModuleConjugate.compute(G.QDBL, a, b);
+		}
+	};
+	
+	@Override
+	public Procedure2<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember> conjugate() {
+		return CONJ;
+	}
+
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64MatrixMember> VDP =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64MatrixMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember in1, QuaternionFloat64RModuleMember in2,
+				QuaternionFloat64MatrixMember out)
+		{
+			directProduct().call(in1, in2, out);
+		}
+	};
+	
+	@Override
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64MatrixMember> vectorDirectProduct() {
+		return VDP;
+	}
+
+	private final Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64MatrixMember> DP =
+			new Procedure3<QuaternionFloat64RModuleMember, QuaternionFloat64RModuleMember, QuaternionFloat64MatrixMember>()
+	{
+		@Override
+		public void call(QuaternionFloat64RModuleMember in1, QuaternionFloat64RModuleMember in2,
+				QuaternionFloat64MatrixMember out)
+		{
+			RModuleDirectProduct.compute(G.QDBL, in1, in2, out);
+		}
+	};
+	
+	@Override
+	public Procedure3<QuaternionFloat64RModuleMember,QuaternionFloat64RModuleMember,QuaternionFloat64MatrixMember> directProduct()
+	{
+		return DP;
 	}
 
 }
