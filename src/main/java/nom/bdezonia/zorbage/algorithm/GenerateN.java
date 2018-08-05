cdf4f0f8a32ddf688326ae16b4e4f623e4c40df9
Barry DeZonia <bdezonia@gmail.com>
2018-08-04 20:40:06 -0500

    Make Generate algorithm support a stride param

--- src/main/java/nom/bdezonia/zorbage/algorithm/GenerateN.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/GenerateN.java
@@ -1,3 +1,29 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package nom.bdezonia.zorbage.algorithm;
 
 import nom.bdezonia.zorbage.procedure.Procedure;
@@ -13,15 +39,17 @@ public class GenerateN {
 	 * @param proc
 	 * @param start
 	 * @param count
+	 * @param stride
 	 * @param storage
 	 */
 	public static <T extends Group<T,U>,U>
-		void compute(T group, Procedure1<U> proc, long start, long count, IndexedDataSource<?,U> storage)
+		void compute(T group, Procedure1<U> proc, long start, long count, long stride, IndexedDataSource<?,U> storage)
 	{
 		U value = group.construct();
-		for (long i = 0; i < count; i++) {
+		for (long i = start, c = 0; c < count; c++) {
 			proc.call(value);
-			storage.set(start+i, value);
+			storage.set(i, value);
+			i += stride;
 		}
 	}
 
@@ -31,17 +59,19 @@ public class GenerateN {
 	 * @param proc
 	 * @param start
 	 * @param count
+	 * @param stride
 	 * @param storage
 	 * @param inputs
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T extends Group<T,U>,U>
-		void compute(T group, Procedure<U> proc, long start, long count, IndexedDataSource<?,U> storage, U... inputs)
+		void compute(T group, Procedure<U> proc, long start, long count, long stride, IndexedDataSource<?,U> storage, U... inputs)
 	{
 		U value = group.construct();
-		for (long i = 0; i < count; i++) {
+		for (long i = start, c = 0; c < count; c++) {
 			proc.call(value, inputs);
-			storage.set(start+i, value);
+			storage.set(i, value);
+			i += stride;
 		}
 	}
 
