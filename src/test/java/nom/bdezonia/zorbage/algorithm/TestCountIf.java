fb6c1216b6d34d2d8296d89e6a8bc0521df67c3c
Barry DeZonia <bdezonia@gmail.com>
2018-08-18 10:40:20 -0500

    Test the CountIf algorithm

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestCountIf.java
@@ -0,0 +1,64 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.condition.GreaterThanConstant;
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Group;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.data.int32.UnsignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestCountIf {
+
+	@Test
+	public void test() {
+		
+		double[] numbers = new double[] {0, 10, 20, 30, 40, 50};
+		
+		IndexedDataSource<?,Float64Member> data = ArrayStorage.allocateDoubles(numbers);
+		
+		UnsignedInt32Member sum = G.UINT32.construct();
+		
+		GreaterThanConstant<Float64Group, Float64Member> condition =
+				new GreaterThanConstant<Float64Group, Float64Member>(G.DBL, new Float64Member(14));
+		
+		CountIf.compute(G.DBL, G.UINT32, condition, data, sum);
+		
+		assertEquals(4, sum.v());
+	}
+}
