ea0bc65ea1566517a20a5ef5df2799efe08038c4
Barry DeZonia <bdezonia@gmail.com>
2018-08-18 00:36:50 -0500

    Improve Fill testing

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestFill.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestFill.java
@@ -72,19 +72,52 @@ public class TestFill {
 	public void test2() {
 
 		Float64Member type = new Float64Member();
+		
 		IndexedDataSource<?, Float64Member> data = Storage.allocate(1000, type);
+		
 		Fill.compute(G.DBL, new Float64Member(17.4), data);
+		data.get(999, type);
+		assertEquals(17.4, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.zero(), data);
+		data.get(999, type);
+		assertEquals(0, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.unity(), data);
+		data.get(999, type);
+		assertEquals(1, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.minBound(), data);
+		data.get(999, type);
+		assertEquals(-Double.MAX_VALUE, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.maxBound(), data);
+		data.get(999, type);
+		assertEquals(Double.MAX_VALUE, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.E(), data);
+		data.get(999, type);
+		assertEquals(2.7182818284590452353602874713526624, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.PI(), data);
+		data.get(999, type);
+		assertEquals(3.1415926535897932384626433832795028, type.v(), 0);
+		
 		Fill.compute(G.DBL, G.DBL.random(), data);
+		
 		Fill.compute(G.DBL, G.DBL.nan(), data);
+		data.get(999, type);
+		assertTrue(Double.isNaN(type.v()));
+		
 		Fill.compute(G.DBL, G.DBL.infinite(), data);
+		data.get(999, type);
+		assertTrue(Double.isInfinite(type.v()));
+		assertTrue(type.v() > 0);
+		
 		Fill.compute(G.DBL, G.DBL.negInfinite(), data);
-		assertTrue(true);
+		data.get(999, type);
+		assertTrue(Double.isInfinite(type.v()));
+		assertTrue(type.v() < 0);
 
 	}
 }
