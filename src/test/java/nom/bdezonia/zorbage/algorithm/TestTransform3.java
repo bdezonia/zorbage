c8e3d9bafb2112af9e924f014bc9749dba9cb6c1
Barry DeZonia <bdezonia@gmail.com>
2018-08-19 13:28:56 -0500

    Rename TestMethodApplication to TestTransform3 and simplify code

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestTransform3.java
@@ -0,0 +1,69 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.impl.Ramp;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Group;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.Storage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestTransform3 {
+	
+	@Test
+	public void test1() {
+		
+		IndexedDataSource<?, Float64Member> data = Storage.allocate(1000, G.DBL.construct());
+		Ramp<Float64Group, Float64Member> ramp =
+				new Ramp<Float64Group, Float64Member>(G.DBL, new Float64Member(), new Float64Member(0.1));
+		Generate.compute(G.DBL, ramp, data);
+		// multiply list1 by list2 and store in list2: with 1 list == squared values in place
+		Transform3.compute(G.DBL, G.DBL.multiply(), data);
+		double tol = 0.0000000000001;
+		Float64Member tmp = G.DBL.construct();
+		data.get(0, tmp);
+		assertEquals(0, tmp.v(), tol);
+		data.get(1, tmp);
+		assertEquals(0.1*0.1, tmp.v(), tol);
+		data.get(2, tmp);
+		assertEquals(0.2*0.2, tmp.v(), tol);
+		data.get(3, tmp);
+		assertEquals(0.3*0.3, tmp.v(), tol);
+		data.get(4, tmp);
+		assertEquals(0.4*0.4, tmp.v(), tol);
+	}
+}
