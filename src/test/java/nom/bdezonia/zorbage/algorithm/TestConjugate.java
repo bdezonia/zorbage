a8e1a441a33cb8273e237d49d8c36e16ecb47aac
Barry DeZonia <bdezonia@gmail.com>
2018-08-21 20:45:41 -0500

    Test Conjugate algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestConjugate.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestConjugate.java
@@ -1,1 +1,72 @@
-package nom.bdezonia.zorbage.algorithm; class TestConjugate {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Procedure1;
+import nom.bdezonia.zorbage.type.data.float64.complex.ComplexFloat64Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestConjugate {
+	
+	public void test() {
+		
+		ComplexFloat64Member tmp = G.CDBL.construct();
+		
+		IndexedDataSource<?,ComplexFloat64Member> list = ArrayStorage.allocate(10, tmp);
+		
+		Procedure1<ComplexFloat64Member> proc = new Procedure1<ComplexFloat64Member>()
+		{
+			private int i = 0;
+			
+			@Override
+			public void call(ComplexFloat64Member a) {
+				a.setR(i);
+				a.setI(i);
+				i++;
+			}
+		};
+		
+		Fill.compute(G.CDBL, proc, list);
+
+		Conjugate.compute(G.CDBL, list, list);
+		
+		for (long i = 0; i < list.size(); i++) {
+			list.get(i, tmp);
+			assertEquals(i, tmp.r(), 0);
+			assertEquals(-i, tmp.i(), 0);
+		}
+	}
+}
