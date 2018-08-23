92fc505c8f431175f219293ddfe8389526b6b29b
Barry DeZonia <bdezonia@gmail.com>
2018-08-22 22:16:19 -0500

    Test MatrixNegate algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestMatrixNegate.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestMatrixNegate.java
@@ -1,1 +1,61 @@
-package nom.bdezonia.zorbage.algorithm; class TestTestMatrixNegate {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64MatrixMember;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestMatrixNegate {
+
+	@Test
+	public void test() {
+		Float64MatrixMember a = new Float64MatrixMember(2, 2, new double[] {1,-2,0,4});
+		Float64MatrixMember b = G.DBL_MAT.construct();
+		G.DBL_MAT.negate().call(a, b);
+		Float64Member value = G.DBL.construct();
+		assertEquals(2, b.rows());
+		assertEquals(2, b.cols());
+		b.v(0, 0, value);
+		assertEquals(-1, value.v(), 0);
+		b.v(0, 1, value);
+		assertEquals(2, value.v(), 0);
+		b.v(1, 0, value);
+		assertEquals(0, value.v(), 0);
+		b.v(1, 1, value);
+		assertEquals(-4, value.v(), 0);
+	}
+}
