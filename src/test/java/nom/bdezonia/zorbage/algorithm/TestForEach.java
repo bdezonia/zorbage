089577fb2661d7f0301964495d0caa28d65b6315
Barry DeZonia <bdezonia@gmail.com>
2018-08-22 20:01:44 -0500

    Test ForEach algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestForEach.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestForEach.java
@@ -1,1 +1,64 @@
-package nom.bdezonia.zorbage.algorithm; class TestForEach {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.data.int8.SignedInt8Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+public class TestForEach {
+	
+	@Test
+	public void test() {
+		IndexedDataSource<?,SignedInt8Member> list = ArrayStorage.allocateBytes(
+				new byte[] {0,1,2,3,0,-1,-2,-3});
+		ForEach.compute(G.INT8, G.INT8.negate(), list);
+		SignedInt8Member value = G.INT8.construct();
+		list.get(0, value);
+		assertEquals(0, value.v());
+		list.get(1, value);
+		assertEquals(-1, value.v());
+		list.get(2, value);
+		assertEquals(-2, value.v());
+		list.get(3, value);
+		assertEquals(-3, value.v());
+		list.get(4, value);
+		assertEquals(0, value.v());
+		list.get(5, value);
+		assertEquals(1, value.v());
+		list.get(6, value);
+		assertEquals(2, value.v());
+		list.get(7, value);
+		assertEquals(3, value.v());
+	}
+}
