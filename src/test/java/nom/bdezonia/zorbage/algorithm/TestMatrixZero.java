3b4637151323a926488fc52f41494ce5649260ff
Barry DeZonia <bdezonia@gmail.com>
2018-08-22 23:19:04 -0500

    Test MatrixZero algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestMatrixZero.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestMatrixZero.java
@@ -1,1 +1,29 @@
-package nom.bdezonia.zorbage.algorithm; class TestMatrixZero {}
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64MatrixMember;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestMatrixZero {
+
+	@Test
+	public void test() {
+		Float64MatrixMember a = new Float64MatrixMember(2,2,new double[] {1,2,3,4});
+		Float64Member value = G.DBL.construct();
+		G.DBL_MAT.zero().call(a);
+		for (int i = 0; i < a.rows(); i++) {
+			for (int j = 0; j < a.cols(); j++) {
+				assertEquals(0, value.v(), 0);
+			}
+		}
+	}
+}
