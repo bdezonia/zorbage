0c61d4d760d8033b966df2f99f7d4bdc5bdb7cc2
Barry DeZonia <bdezonia@gmail.com>
2018-08-22 19:55:19 -0500

    Test FindMedianFraction algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestFindMedianFraction.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestFindMedianFraction.java
@@ -1,1 +1,62 @@
-package nom.bdezonia.zorbage.algorithm; class TestFindMedianFraction {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.int8.SignedInt8Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestFindMedianFraction {
+
+	@Test
+	public void test() {
+		SignedInt8Member numer = G.INT8.construct();
+		SignedInt8Member denom = G.INT8.construct();
+
+		IndexedDataSource<?,SignedInt8Member> list1 = ArrayStorage.allocateBytes(
+				new byte[] {1,2,3});
+		FindMedianFraction.compute(G.INT8, list1, numer, denom);
+		assertEquals(2, numer.v());
+		assertEquals(1, denom.v());
+
+		IndexedDataSource<?,SignedInt8Member> list2 = ArrayStorage.allocateBytes(
+				new byte[] {1,2,3,4});
+		FindMedianFraction.compute(G.INT8, list2, numer, denom);
+		assertEquals(5, numer.v());
+		assertEquals(2, denom.v());
+	}
+}
