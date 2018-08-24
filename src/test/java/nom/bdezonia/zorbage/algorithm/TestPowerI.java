ef15e58a870d7472808a70dbe1ef23c2a59d2147
Barry DeZonia <bdezonia@gmail.com>
2018-08-23 20:50:43 -0500

    Test PowerI algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestPowerI.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestPowerI.java
@@ -1,1 +1,64 @@
-package nom.bdezonia.zorbage.algorithm; class TestPowerI {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestPowerI {
+
+	@Test
+	public void test() {
+		SignedInt32Member a = G.INT32.construct("4");
+		SignedInt32Member b = G.INT32.construct();
+
+		PowerI.compute(G.INT32, 3, a, b);
+		assertEquals(4*4*4, b.v(), 0);
+		
+		try {
+			PowerI.compute(G.INT32, -3, a, b);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+
+		PowerI.compute(G.INT32, 0, a, b);
+		assertEquals(1, b.v(), 0);
+	}
+}
