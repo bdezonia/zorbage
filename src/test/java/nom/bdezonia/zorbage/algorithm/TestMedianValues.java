1f8485999e4ea19503e28ff670c43328a8494b08
Barry DeZonia <bdezonia@gmail.com>
2018-08-23 19:53:56 -0500

    Test MedianValues algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestMedianValues.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestMedianValues.java
@@ -1,1 +1,63 @@
-package nom.bdezonia.zorbage.algorithm; class TestMedianValues {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestMedianValues {
+	
+	@Test
+	public void test() {
+		SignedInt32Member result1 = G.INT32.construct();
+		SignedInt32Member result2 = G.INT32.construct();
+		
+		IndexedDataSource<?,SignedInt32Member> list1 = ArrayStorage.allocateInts(
+				new int[] {1,77,1000,44,19,6});
+		MedianValues.compute(G.INT32, list1, result1, result2);
+		assertTrue(result1.v() != result2.v());
+		assertTrue(result1.v() == 19 || result1.v() == 44);
+		assertTrue(result2.v() == 19 || result2.v() == 44);
+
+		IndexedDataSource<?,SignedInt32Member> list2 = ArrayStorage.allocateInts(
+				new int[] {77,1000,44,19,6});
+		MedianValues.compute(G.INT32, list2, result1, result2);
+		assertTrue(result1.v() == result2.v());
+		assertTrue(result1.v() == 44 && result2.v() == 44);
+	}
+}
