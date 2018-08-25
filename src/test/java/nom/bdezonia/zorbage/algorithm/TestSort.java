ec37afb9b12471c2fed717b177990e54cf3f192c
Barry DeZonia <bdezonia@gmail.com>
2018-08-24 19:41:00 -0500

    Test Sort algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestSort.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestSort.java
@@ -1,1 +1,91 @@
-package nom.bdezonia.zorbage.algorithm; class TestSort {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestSort {
+
+	@Test
+	public void test() {
+		IndexedDataSource<?,SignedInt32Member> storage = ArrayStorage.allocateInts(
+				new int[] {6,3,99,-1,66,-50,0,0,3});
+		SignedInt32Member value = G.INT32.construct();
+		
+		Sort.compute(G.INT32, storage);
+		storage.get(0, value);
+		assertEquals(-50, value.v());
+		storage.get(1, value);
+		assertEquals(-1, value.v());
+		storage.get(2, value);
+		assertEquals(0, value.v());
+		storage.get(3, value);
+		assertEquals(0, value.v());
+		storage.get(4, value);
+		assertEquals(3, value.v());
+		storage.get(5, value);
+		assertEquals(3, value.v());
+		storage.get(6, value);
+		assertEquals(6, value.v());
+		storage.get(7, value);
+		assertEquals(66, value.v());
+		storage.get(8, value);
+		assertEquals(99, value.v());
+		
+		Sort.compute(G.INT32, storage);
+		storage.get(0, value);
+		assertEquals(-50, value.v());
+		storage.get(1, value);
+		assertEquals(-1, value.v());
+		storage.get(2, value);
+		assertEquals(0, value.v());
+		storage.get(3, value);
+		assertEquals(0, value.v());
+		storage.get(4, value);
+		assertEquals(3, value.v());
+		storage.get(5, value);
+		assertEquals(3, value.v());
+		storage.get(6, value);
+		assertEquals(6, value.v());
+		storage.get(7, value);
+		assertEquals(66, value.v());
+		storage.get(8, value);
+		assertEquals(99, value.v());
+	}
+}
