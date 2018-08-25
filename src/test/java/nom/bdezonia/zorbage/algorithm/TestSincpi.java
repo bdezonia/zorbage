3b249fbf6aa8e3ae21c63adbbb2e17c350b62322
Barry DeZonia <bdezonia@gmail.com>
2018-08-24 19:34:50 -0500

    Test Sincpi algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestSincpi.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestSincpi.java
@@ -1,1 +1,81 @@
-package nom.bdezonia.zorbage.algorithm; class TestSincpi {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestSincpi {
+
+	@Test
+	public void test() {
+		Float64Member x = G.DBL.construct("111");
+		Float64Member result = G.DBL.construct("999");
+		
+		x.setV(0);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(1, result.v(), 0);
+		
+		x.setV(1.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(Math.PI/4)/(Math.PI/4), result.v(), 0);
+		
+		x.setV(2.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(Math.PI/2)/(Math.PI/2), result.v(), 0);
+		
+		x.setV(3.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(3*Math.PI/4)/(3*Math.PI/4), result.v(), 0);
+		
+		x.setV(4.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(Math.PI)/(Math.PI), result.v(), 0);
+
+		x.setV(5.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(5*Math.PI/4)/(5*Math.PI/4), result.v(), 0);
+		
+		x.setV(6.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(3*Math.PI/2)/(3*Math.PI/2), result.v(), 0);
+
+		x.setV(7.0/4);
+		Sincpi.compute(G.DBL, x, result);
+		assertEquals(Math.sin(7*Math.PI/4)/(7*Math.PI/4), result.v(), 0);
+		
+	}
+}
