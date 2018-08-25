8e32d565d356bcd0e57ceb37393c3bd2b140aa2e
Barry DeZonia <bdezonia@gmail.com>
2018-08-24 20:20:19 -0500

    Test TwoElementFraction algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestTwoElementFraction.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestTwoElementFraction.java
@@ -1,1 +1,57 @@
-package nom.bdezonia.zorbage.algorithm; class TestTwoElementFraction {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestTwoElementFraction {
+
+	@Test
+	public void test() {
+		Float64Member result1 = G.DBL.construct("4");
+		Float64Member result2a = G.DBL.construct("7");
+		Float64Member result2b = G.DBL.construct(result2a);
+		Float64Member numer = G.DBL.construct();
+		Float64Member denom = G.DBL.construct();
+		TwoElementFraction.compute(G.DBL, result1, result2a, numer, denom);
+		assertEquals(result1.v()+result2a.v(), numer.v(), 0);
+		assertEquals(2, denom.v(), 0);
+		TwoElementFraction.compute(G.DBL, result2a, result2b, numer, denom);
+		assertEquals(result2a.v(), numer.v(), 0);
+		assertEquals(1, denom.v(), 0);
+	}
+}
