72c22bf173fa5d307a27c77fd536d44b5539802f
Barry DeZonia <bdezonia@gmail.com>
2018-08-24 20:37:04 -0500

    Test ParallelTransform4 algorithm

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestParallelTransform4.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestParallelTransform4.java
@@ -1,1 +1,68 @@
-package nom.bdezonia.zorbage.algorithm; class TestParallelTransform4 {}
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.procedure.Procedure4;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.IndexedDataSource;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestParallelTransform4 {
+
+	@Test
+	public void test() {
+		
+		IndexedDataSource<?,SignedInt32Member> a = ArrayStorage.allocateInts(
+				new int[] {1,2,3,4,5});
+		SignedInt32Member value = G.INT32.construct();
+		Procedure4<SignedInt32Member,SignedInt32Member,SignedInt32Member,SignedInt32Member> proc =
+				new Procedure4<SignedInt32Member,SignedInt32Member, SignedInt32Member, SignedInt32Member>()
+		{
+			@Override
+			public void call(SignedInt32Member a, SignedInt32Member b, SignedInt32Member c, SignedInt32Member d) {
+				d.setV(9);
+			}
+		};
+		
+		ParallelTransform4.compute(G.INT32, proc, 0, 0, 0, 0, a.size(), 1, 1, 1, 1, a, a, a, a);
+		
+		for (int i = 0; i < a.size(); i++) {
+			a.get(i, value);
+			assertEquals(9, value.v());
+		}
+	}
+}
