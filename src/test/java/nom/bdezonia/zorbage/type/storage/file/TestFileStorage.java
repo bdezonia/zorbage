879831f02ae692883414eb9ed4e490ac34f6ff03
Barry DeZonia <bdezonia@gmail.com>
2018-09-01 14:40:37 -0500

    Make a testing package name match a source package name

--- src/test/java/nom/bdezonia/zorbage/type/storage/file/TestFileStorage.java
+++ src/test/java/nom/bdezonia/zorbage/type/storage/file/TestFileStorage.java
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package nom.bdezonia.zorbage.type.storage.linear.file;
+package nom.bdezonia.zorbage.type.storage.file;
 
 import static org.junit.Assert.assertEquals;
 
