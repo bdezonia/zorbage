557d666f7de7f0fea20655bf62e6ce4d82a5db6e
Barry DeZonia <bdezonia@gmail.com>
2018-09-01 21:53:48 -0500

    Further test prep

--- src/test/java/nom/bdezonia/zorbage/type/data/universal/TestTensorStringRepresentation.java
+++ src/test/java/nom/bdezonia/zorbage/type/data/universal/TestTensorStringRepresentation.java
@@ -936,31 +936,31 @@ public class TestTensorStringRepresentation {
 
 	@Test
 	public void test3d2x2x1() {
-		TensorStringRepresentation a = new TensorStringRepresentation("[[[1][1]][[1][1]]]");
-		TensorStringRepresentation b = new TensorStringRepresentation("[[[{1}][{1}]][[{1}][{1}]]]");
-		TensorStringRepresentation c = new TensorStringRepresentation("[[[{1,1}][{1,1}]][[{1,1}][{1,1}]]]");
-		TensorStringRepresentation d = new TensorStringRepresentation("[[[{1,1,1}][{1,1,1}]][[{1,1,1}][{1,1,1}]]]");
-		TensorStringRepresentation e = new TensorStringRepresentation("[[[{1,1,1,1}][{1,1,1,1}]][[{1,1,1,1}][{1,1,1,1}]]]");
-		TensorStringRepresentation f = new TensorStringRepresentation("[[[{1,1,1,1,1}][{1,1,1,1,1}]][[{1,1,1,1,1}][{1,1,1,1,1}]]]");
-		TensorStringRepresentation g = new TensorStringRepresentation("[[[{1,1,1,1,1,1}][{1,1,1,1,1,1}]][[{1,1,1,1,1,1}][{1,1,1,1,1,1}]]]");
-		TensorStringRepresentation h = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1}][{1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1}][{1,1,1,1,1,1,1}]]]");
-		TensorStringRepresentation i = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1}]]]");
-		TensorStringRepresentation j = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1,1}]]]");
+		TensorStringRepresentation a = new TensorStringRepresentation("[[[1][2]][[3][4]]]");
+		TensorStringRepresentation b = new TensorStringRepresentation("[[[{1}][{2}]][[{3}][{4}]]]");
+		TensorStringRepresentation c = new TensorStringRepresentation("[[[{1,2}][{3,4}]][[{5,6}][{7,8}]]]");
+		TensorStringRepresentation d = new TensorStringRepresentation("[[[{1,2,3}][{4,5,6}]][[{7,8,9}][{10,11,12}]]]");
+		TensorStringRepresentation e = new TensorStringRepresentation("[[[{1,2,3,4}][{5,6,7,8}]][[{9,10,11,12}][{13,14,15,16}]]]");
+		TensorStringRepresentation f = new TensorStringRepresentation("[[[{1,2,3,4,5}][{6,7,8,9,10}]][[{11,12,13,14,15}][{16,17,18,19,20}]]]");
+		TensorStringRepresentation g = new TensorStringRepresentation("[[[{1,2,3,4,5,6}][{7,8,9,10,11,12}]][[{13,14,15,16,17,18}][{19,20,21,22,23,24}]]]");
+		TensorStringRepresentation h = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7}][{8,9,10,11,12,13,14}]][[{15,16,17,18,19,20,21}][{22,23,24,25,26,27,28}]]]");
+		TensorStringRepresentation i = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7,8}][{9,10,11,12,13,14,15,16}]][[{17,18,19,20,21,22,23,24}][{25,26,27,28,29,30,31,32}]]]");
+		TensorStringRepresentation j = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7,8,9}][{10,11,12,13,14,15,16,17,18}]][[{19,20,21,22,23,24,25,26,27}][{28,29,30,31,32,33,34,35,36}]]]");
 		assertTrue(true);
 	}
 
 	@Test
 	public void test3d2x2x2() {
-		TensorStringRepresentation a = new TensorStringRepresentation("[[[1,1][1,1]][[1,1][1,1]]]");
-		TensorStringRepresentation b = new TensorStringRepresentation("[[[{1},{1}][{1},{1}]][[{1},{1}][{1},{1}]]]");
-		TensorStringRepresentation c = new TensorStringRepresentation("[[[{1,1},{1,1}][{1,1},{1,1}]][[{1,1},{1,1}][{1,1},{1,1}]]]");
-		TensorStringRepresentation d = new TensorStringRepresentation("[[[{1,1,1},{1,1,1}][{1,1,1},{1,1,1}]][[{1,1,1},{1,1,1}][{1,1,1},{1,1,1}]]]");
-		TensorStringRepresentation e = new TensorStringRepresentation("[[[{1,1,1,1},{1,1,1,1}][{1,1,1,1},{1,1,1,1}]][[{1,1,1,1},{1,1,1,1}][{1,1,1,1},{1,1,1,1}]]]");
-		TensorStringRepresentation f = new TensorStringRepresentation("[[[{1,1,1,1,1},{1,1,1,1,1}][{1,1,1,1,1},{1,1,1,1,1}]][[{1,1,1,1,1},{1,1,1,1,1}][{1,1,1,1,1},{1,1,1,1,1}]]]");
-		TensorStringRepresentation g = new TensorStringRepresentation("[[[{1,1,1,1,1,1},{1,1,1,1,1,1}][{1,1,1,1,1,1},{1,1,1,1,1,1}]][[{1,1,1,1,1,1},{1,1,1,1,1,1}][{1,1,1,1,1,1},{1,1,1,1,1,1}]]]");
-		TensorStringRepresentation h = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1},{1,1,1,1,1,1,1}][{1,1,1,1,1,1,1},{1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1},{1,1,1,1,1,1,1}][{1,1,1,1,1,1,1},{1,1,1,1,1,1,1}]]]");
-		TensorStringRepresentation i = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}]]]");
-		TensorStringRepresentation j = new TensorStringRepresentation("[[[{1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1}]][[{1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1}][{1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1}]]]");
+		TensorStringRepresentation a = new TensorStringRepresentation("[[[1,2][3,4]][[5,6][7,8]]]");
+		TensorStringRepresentation b = new TensorStringRepresentation("[[[{1},{2}][{3},{4}]][[{5},{6}][{7},{8}]]]");
+		TensorStringRepresentation c = new TensorStringRepresentation("[[[{1,2},{3,4}][{5,6},{7,8}]][[{9,10},{11,12}][{13,14},{15,16}]]]");
+		TensorStringRepresentation d = new TensorStringRepresentation("[[[{1,2,3},{4,5,6}][{7,8,9},{10,11,12}]][[{13,14,15},{16,17,18}][{19,20,21},{22,23,24}]]]");
+		TensorStringRepresentation e = new TensorStringRepresentation("[[[{1,2,3,4},{5,6,7,8}][{9,10,11,12},{13,14,15,16}]][[{17,18,19,20},{21,22,23,24}][{25,26,27,28},{29,30,31,32}]]]");
+		TensorStringRepresentation f = new TensorStringRepresentation("[[[{1,2,3,4,5},{6,7,8,9,10}][{11,12,13,14,15},{16,17,18,19,20}]][[{21,22,23,24,25},{26,27,28,29,30}][{31,32,33,34,35},{36,37,38,39,40}]]]");
+		TensorStringRepresentation g = new TensorStringRepresentation("[[[{1,2,3,4,5,6},{7,8,9,10,11,12}][{13,14,15,16,17,18},{19,20,21,22,23,24}]][[{25,26,27,28,29,30},{31,32,33,34,35,36}][{37,38,39,40,41,42},{43,44,45,46,47,48}]]]");
+		TensorStringRepresentation h = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7},{8,9,10,11,12,13,14}][{15,16,17,18,19,20,21},{22,23,24,25,26,27,28}]][[{29,30,31,32,33,34,35},{36,37,38,39,40,41,42}][{43,44,45,46,47,48,49},{50,51,52,53,54,55,56}]]]");
+		TensorStringRepresentation i = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7,8},{9,10,11,12,13,14,15,16}][{17,18,19,20,21,22,23,24},{25,26,27,28,29,30,31,32}]][[{33,34,35,36,37,38,39,40},{41,42,43,44,45,46,47,48}][{49,50,51,52,53,54,55,56},{57,58,59,60,61,62,63,64}]]]");
+		TensorStringRepresentation j = new TensorStringRepresentation("[[[{1,2,3,4,5,6,7,8,9},{10,11,12,13,14,15,16,17,18}][{19,20,21,22,23,24,25,26,27},{28,29,30,31,32,33,34,35,36}]][[{37,38,39,40,41,42,43,44,45},{46,47,48,49,50,51,52,53,54}][{55,56,57,58,59,60,61,62,63},{64,65,66,67,68,69,70,71,72}]]]");
 		assertTrue(true);
 	}
 
