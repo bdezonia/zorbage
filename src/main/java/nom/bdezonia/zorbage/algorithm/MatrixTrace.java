443fafc480252767afc05b17e806f6ee228409e8
Barry DeZonia <bdezonia@gmail.com>
2018-11-18 20:13:47 -0600

    Define a matrix trace computation algorithm

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/MatrixTrace.java
@@ -0,0 +1,61 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.algebra.MatrixMember;
+import nom.bdezonia.zorbage.type.data.helper.MatrixDiagonalRModuleBridge;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class MatrixTrace {
+
+	private MatrixTrace() { }
+	
+	/**
+	 * 
+	 * @param group
+	 * @param matrix
+	 * @param result
+	 */
+	public static <T extends Group<T,U> & Addition<U>,U>
+		void compute(T group, MatrixMember<U> matrix, U result)
+	{
+		MatrixDiagonalRModuleBridge<U> diag = new MatrixDiagonalRModuleBridge<U>(group, matrix);
+		// TODO: make this into a RModuleSum algorithm and just call it here instead of calcing.
+		U value = group.construct();
+		group.zero().call(result);
+		for (long i = 0; i < diag.length(); i++) {
+			diag.v(i, value);
+			group.add().call(result, value, result);
+		}
+	}
+}
