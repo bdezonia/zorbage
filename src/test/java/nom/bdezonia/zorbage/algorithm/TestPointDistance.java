929ce9b457a82f7b6ac53cd26cd481633869bbb7
Barry DeZonia <bdezonia@gmail.com>
2018-12-01 19:12:13 -0600

    Define test code for PointDistance

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestPointDistance.java
@@ -0,0 +1,71 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.data.point.Point;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestPointDistance {
+
+	@Test
+	public void run() {
+		
+		Point a = new Point(2);
+		a.setComponent(0, 1);
+		a.setComponent(1, 1);
+
+		Point b = new Point(2);
+		b.setComponent(0, 3);
+		b.setComponent(1, 4);
+
+		Point c = new Point(3);
+		
+		Float64Member result = G.DBL.construct();
+		
+		PointDistance.compute(a, b, result);
+		assertEquals(Math.sqrt(13), result.v(), 0.000000000000001);
+		
+		try {
+			PointDistance.compute(a, c, result);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+}
