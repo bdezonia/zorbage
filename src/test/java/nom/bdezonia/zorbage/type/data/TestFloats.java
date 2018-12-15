3ad8a8be6e9933b3c1b5935adc21b67d169eeb3b
Barry DeZonia <bdezonia@gmail.com>
2018-12-15 00:35:47 -0600

    Define a simple test that exercises the 3 floating types

---
+++ src/test/java/nom/bdezonia/zorbage/type/data/TestFloats.java
@@ -0,0 +1,74 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2018 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.data;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.groups.G;
+import nom.bdezonia.zorbage.sampling.IntegerIndex;
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Group;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.data.universal.PrimitiveConversion;
+import nom.bdezonia.zorbage.type.data.universal.PrimitiveConverter;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestFloats {
+
+	private <T extends Group<T,U> & Addition<U>, U extends PrimitiveConversion>
+		void test(T group)
+	{
+		Float64Member dbl = G.DBL.construct();
+		U val1 = group.construct("1.4");
+		U val2 = group.construct("5.3");
+		U val3 = group.construct();
+		group.add().call(val1, val2, val3);
+		PrimitiveConverter.convert(new IntegerIndex(0), new IntegerIndex(0), new IntegerIndex(0), val3, dbl);
+		assertEquals(6.7, dbl.v(), 0.01);
+	}
+	
+	@Test
+	public void testF16() {
+		test(G.HLF);
+	}
+	
+	@Test
+	public void testF32() {
+		test(G.FLT);
+	}
+
+	@Test
+	public void testF64() {
+		test(G.DBL);
+	}
+}
