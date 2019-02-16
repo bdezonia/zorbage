de43ff1c90b10596769909bf18805cbb72a4799d
Barry DeZonia <bdezonia@gmail.com>
2019-02-16 01:23:03 -0600

    Experiment a bit with procedures that work with derivatives

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Derivative.java
@@ -0,0 +1,71 @@
+/*
+ * Zorbage: an Algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Derivative<T extends Algebra<T,U> & Addition<U> & Invertible<U>,U>
+	implements Procedure2<U,U>
+{
+	private final T alg;
+	private final Procedure2<U,U> proc;
+	private final U delta;
+
+	/**
+	 * 
+	 * @param alg
+	 * @param proc
+	 * @param delta
+	 */
+	public Derivative(T alg, Procedure2<U,U> proc, U delta) {
+		this.alg = alg;
+		this.proc = proc;
+		this.delta = alg.construct(delta);
+	}
+
+	@Override
+	public void call(U point1, U result)
+	{
+		U t = alg.construct();
+		U tmp1 = alg.construct();
+		U tmp2 = alg.construct();
+		U point2 = alg.construct();
+		proc.call(point1, tmp1);
+		alg.add().call(point1, delta, point2);
+		proc.call(point2, tmp2);
+		alg.subtract().call(tmp2, tmp1, t);
+		alg.divide().call(t, delta, result);
+	}
+}
---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/NewtonsMethod.java
@@ -0,0 +1,78 @@
+/*
+ * Zorbage: an Algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class NewtonsMethod<T extends Algebra<T,U> & Addition<U> & Invertible<U>, U>
+	implements Procedure2<U,U>
+{
+	// x1 = x0 - f(x0) / f'(x0)
+	// iterate
+
+	private final T alg;
+	private final Procedure2<U,U> f;
+	private final Derivative<T,U> fPrime;
+	private final long maxIters;
+
+	/**
+	 * 
+	 * @param alg
+	 * @param f
+	 * @param delta
+	 * @param maxIters
+	 */
+	public NewtonsMethod(T alg, Procedure2<U,U> f, U delta, long maxIters) {
+		this.alg = alg;
+		this.f = f;
+		this.fPrime = new Derivative<T,U>(alg, f, delta);
+		this.maxIters = maxIters;
+	}
+	
+	@Override
+	public void call(U guess, U result) {
+		U tmp = alg.construct(guess);
+		U n = alg.construct();
+		U d = alg.construct();
+		U correction = alg.construct();
+		for (long i = 0; i < maxIters; i++) {
+			f.call(tmp, n);
+			fPrime.call(tmp, d);
+			alg.divide().call(n, d, correction);
+			alg.subtract().call(tmp, correction, tmp);
+		}
+		alg.assign().call(tmp, result);
+	}
+}
