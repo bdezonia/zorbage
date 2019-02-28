c9e8bb2f1b599b5b3303479f7d6bd5ad6d97c28b
Barry DeZonia <bdezonia@gmail.com>
2019-02-27 21:10:25 -0600

    Test NewtonsMethod
    Ideally we should improve the testing for failure to converge when
    given a bad guess. We'll leave that for a later fix.

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestNewtonsMethod.java
@@ -0,0 +1,68 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.function.Function2;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Algebra;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+
+/**
+ * 
+ * @author Barry Dezonia
+ *
+ */
+public class TestNewtonsMethod {
+
+	@Test
+	public void test() {
+		Float64Member delta = G.DBL.construct("0.0001");
+		Float64Member guess = G.DBL.construct("-1");
+		Float64Member result = G.DBL.construct();
+		NewtonsMethod<Float64Algebra,Float64Member> method = new NewtonsMethod<Float64Algebra,Float64Member>(G.DBL, eqn, delta, 10);
+		if (method.call(guess, result))
+			assertEquals(-1.324717, result.v(), 0.00001);
+		else
+			fail();
+	}
+	
+	private Procedure2<Float64Member, Float64Member> eqn = new Procedure2<Float64Member, Float64Member>() {
+		
+		@Override
+		public void call(Float64Member a, Float64Member b) {
+			double v = a.v();
+			v = v*v*v - v + 1;
+			b.setV(v);
+		}
+	};
+}
