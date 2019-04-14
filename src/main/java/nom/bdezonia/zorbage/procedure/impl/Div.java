05e07d4bb47803327483629563866246923dfc11
Barry DeZonia <bdezonia@gmail.com>
2019-04-13 19:37:00 -0500

    Flesh out parser support methods

---
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/Div.java
@@ -0,0 +1,53 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.procedure.impl;
+
+import nom.bdezonia.zorbage.procedure.Procedure3;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.IntegralDivision;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Div<T extends Algebra<T,U> & IntegralDivision<U>,U>
+	implements Procedure3<U,U,U>
+{
+	private final T algebra;
+	
+	public Div(T algebra) {
+		this.algebra = algebra;
+	}
+
+	@Override
+	public void call(U a, U b, U c) {
+		algebra.div().call(a, b, c);
+	}
+
+}
---
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/ModL.java
@@ -0,0 +1,64 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.procedure.impl;
+
+import nom.bdezonia.zorbage.procedure.Procedure;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.IntegralDivision;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class ModL<T extends Algebra<T,U> & IntegralDivision<U>, U>
+	implements Procedure<U>
+{
+	private final Procedure<U> ancestor1;
+	private final Procedure<U> ancestor2;
+	private final Mod<T,U> lowerProc;
+	private final U tmp1;
+	private final U tmp2;
+	
+	public ModL(T algebra, Procedure<U> ancestor1, Procedure<U> ancestor2) {
+		this.ancestor1 = ancestor1;
+		this.ancestor2 = ancestor2;
+		this.lowerProc = new Mod<T,U>(algebra);
+		tmp1 = algebra.construct();
+		tmp2 = algebra.construct();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void call(U result, U... inputs) {
+		ancestor1.call(tmp1, inputs);
+		ancestor2.call(tmp2, inputs);
+		lowerProc.call(tmp1, tmp2, result);
+	}
+
+}
