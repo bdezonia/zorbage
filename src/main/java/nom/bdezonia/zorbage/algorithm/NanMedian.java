9e744286390f09a7970380b6835deb4875326800
Barry DeZonia <bdezonia@gmail.com>
2019-05-12 13:27:00 -0500

    Add NanMedian algorithm: the median of the non-NaN values in a list

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/NanMedian.java
@@ -0,0 +1,85 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Invertible;
+import nom.bdezonia.zorbage.type.algebra.NaN;
+import nom.bdezonia.zorbage.type.algebra.Ordered;
+import nom.bdezonia.zorbage.type.algebra.Unity;
+import nom.bdezonia.zorbage.type.ctor.Allocatable;
+import nom.bdezonia.zorbage.type.storage.Storage;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class NanMedian {
+
+	private NanMedian() {}
+
+	/**
+	 * 
+	 * @param alg
+	 * @param storage
+	 * @param result
+	 */
+	public static <T extends Algebra<T,U> & Addition<U> & Invertible<U> & Ordered<U> &
+								Unity<U> & NaN<U>,
+					U extends Allocatable<U>>
+		void compute(T alg, IndexedDataSource<U> storage, U result)
+	{
+		U value = alg.construct();
+		long valueCount = 0;
+		long sz = storage.size();
+		for (long i = 0; i < sz; i++) {
+			storage.get(i, value);
+			if (!alg.isNaN().call(value))
+				valueCount++;
+		}
+		if (valueCount == 0)
+			alg.nan().call(result);
+		else {
+			IndexedDataSource<U> filteredValues = Storage.allocate(valueCount, result);
+			valueCount = 0;
+			for (long i = 0; i < sz; i++) {
+				storage.get(i, value);
+				if (!alg.isNaN().call(value)) {
+					filteredValues.set(valueCount, value);
+					valueCount++;
+				}
+			}
+			U numer = alg.construct();
+			U denom = alg.construct();
+			FindMedianFraction.compute(alg, filteredValues, numer, denom);
+			alg.divide().call(numer, denom, result);
+		}
+	}
+}
