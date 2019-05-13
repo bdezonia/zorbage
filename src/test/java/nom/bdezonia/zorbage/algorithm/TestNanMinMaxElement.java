eecc92e88edf1e98d898eeb8669c3529482d9011
Barry DeZonia <bdezonia@gmail.com>
2019-05-12 19:23:26 -0500

    Further flesh out NaN query api

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestNanMinMaxElement.java
@@ -0,0 +1,63 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.data.int64.SignedInt64Member;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestNanMinMaxElement {
+
+	@Test
+	public void test() {
+		Float64Member min = G.DBL.construct();
+		Float64Member max = G.DBL.construct();
+		IndexedDataSource<Float64Member> storage = null;
+		
+		storage = ArrayStorage.allocateDoubles(new double[]{-16,20,7,-55,0,1,5,74,44,13});
+		NanMinMaxElement.compute(G.DBL, storage, min, max);
+		assertEquals(-55, min.v(), 0);
+		assertEquals(74, max.v(), 0);
+		
+		storage = ArrayStorage.allocateDoubles(new double[]{Double.NaN,-16,20,7,Double.NaN,-55,0,1,Double.NaN,5,74,44,13,Double.NaN});
+		NanMinMaxElement.compute(G.DBL, storage, min, max);
+		assertEquals(-55, min.v(), 0);
+		assertEquals(74, max.v(), 0);
+	}
+
+}
