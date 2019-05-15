0f16e6d86a7e4aa199e58cd63b46b05f021e1793
Barry DeZonia <bdezonia@gmail.com>
2019-05-15 18:09:17 -0500

    Eliminate warnings

--- src/test/java/nom/bdezonia/zorbage/algorithm/TestIsSortedUntil.java
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestIsSortedUntil.java
@@ -27,8 +27,6 @@
 package nom.bdezonia.zorbage.algorithm;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
