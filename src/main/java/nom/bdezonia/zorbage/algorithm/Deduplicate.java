86a6d1699dfb6051689239446eab49a0e16962d4
Barry DeZonia <bdezonia@gmail.com>
2019-05-23 20:22:45 -0500

    Refactor some code to a new Deduplicate algorithm

--- src/main/java/nom/bdezonia/zorbage/algorithm/Deduplicate.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Deduplicate.java
@@ -27,10 +27,8 @@
 package nom.bdezonia.zorbage.algorithm;
 
 import nom.bdezonia.zorbage.type.algebra.Algebra;
-import nom.bdezonia.zorbage.type.algebra.Ordered;
 import nom.bdezonia.zorbage.type.ctor.Allocatable;
 import nom.bdezonia.zorbage.type.storage.Storage;
-import nom.bdezonia.zorbage.type.storage.datasource.ConcatenatedDataSource;
 import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
 
 /**
@@ -38,26 +36,65 @@ import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
  * @author Barry DeZonia
  *
  */
-public class SetUnion {
+public class Deduplicate {
 
 	/**
-	 * SetUnion : calculate the set union of two possibly unsorted lists.
-	 * The resulting data source has no duplicates and is sorted.
 	 * 
 	 * @param alg
 	 * @param a
-	 * @param b
 	 * @return
 	 */
-	public static <T extends Algebra<T,U> & Ordered<U>, U extends Allocatable<U>>
-		IndexedDataSource<U> compute(T alg, IndexedDataSource<U> a, IndexedDataSource<U> b)
+	public static <T extends Algebra<T,U>, U extends Allocatable<U>>
+		IndexedDataSource<U> compute(T alg, IndexedDataSource<U> a)
 	{
 		U type = alg.construct();
-		IndexedDataSource<U> combined = new ConcatenatedDataSource<>(a, b);
-		IndexedDataSource<U> tmpList = Storage.allocate(combined.size(), type);
-		Copy.compute(alg, combined, tmpList);
-		Sort.compute(alg, tmpList);
-		return Deduplicate.compute(alg, tmpList);
+		long dupeCount = countDupes(alg, a);
+		long resultSize = a.size() - dupeCount;
+		IndexedDataSource<U> result = Storage.allocate(resultSize, type);
+		copyWithoutDupes(alg, a, result);
+		return result;
+	}
+
+	private static <T extends Algebra<T,U>, U>
+		long countDupes(T alg, IndexedDataSource<U> a)
+	{
+		long aSize = a.size();
+		if (aSize <= 1)
+			return 0;
+		U tmp1 = alg.construct();
+		U tmp2 = alg.construct();
+		a.get(0, tmp1);
+		long count = 0;
+		long ai = 1;
+		while (ai < aSize) {
+			a.get(ai++, tmp2);
+			if (alg.isEqual().call(tmp1, tmp2))
+				count++;
+			alg.assign().call(tmp2, tmp1);
+		}
+		return count;
 	}
 	
+	private static <T extends Algebra<T,U>, U>
+		void copyWithoutDupes(T alg, IndexedDataSource<U> a, IndexedDataSource<U> b)
+	{
+		long aSize = a.size();
+		if (aSize <= 1) {
+			Copy.compute(alg, a, b);
+			return;
+		}
+		U tmp1 = alg.construct();
+		U tmp2 = alg.construct();
+		a.get(0, tmp1);
+		b.set(0, tmp1);
+		long ai = 1;
+		long bi = 1;
+		while (ai < aSize) {
+			a.get(ai++, tmp2);
+			if (alg.isNotEqual().call(tmp1, tmp2)) {
+				b.set(bi++, tmp2);
+			}
+			alg.assign().call(tmp2, tmp1);
+		}
+	}
 }
