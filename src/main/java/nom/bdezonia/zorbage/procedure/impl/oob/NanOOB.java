8c9ff4275c223652c9a38b0f5859a8bb7c745755
Barry DeZonia <bdezonia@gmail.com>
2019-07-13 12:46:45 -0500

    Add another OOB behavior: Nan values

---
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/oob/NanOOB.java
@@ -0,0 +1,64 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.procedure.impl.oob;
+
+import nom.bdezonia.zorbage.procedure.Procedure2;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.NaN;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class NanOOB<T extends Algebra<T,U> & NaN<U>, U> implements Procedure2<Long,U> {
+
+	private final T alg;
+	private final long length;
+	private final U nan;
+
+	/**
+	 * 
+	 * @param alg
+	 * @param a
+	 */
+	public NanOOB(T alg, long length) {
+		this.alg = alg;
+		this.length = length;
+		this.nan = alg.construct();
+		alg.nan().call(nan);
+	}
+
+	@Override
+	public void call(Long i, U value) {
+		if (i < 0 || i >= length)
+			alg.assign().call(nan, value);
+		else
+			throw new IllegalArgumentException("OOB method called with in bounds index");
+	}
+
+}
