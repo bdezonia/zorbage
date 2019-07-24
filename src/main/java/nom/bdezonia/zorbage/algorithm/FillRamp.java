e65e5c7038f1a072f3e276ab1f69b1dcb436cdb5
Barry DeZonia <bdezonia@gmail.com>
2019-07-23 20:07:47 -0500

    Add a FillRamp algorithm

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/FillRamp.java
@@ -0,0 +1,63 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Addition;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Unity;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class FillRamp {
+
+	/**
+	 * 
+	 * @param alg
+	 * @param startVal
+	 * @param add
+	 * @param data
+	 */
+	public static <T extends Algebra<T,U> & Addition<U> & Unity<U>, U>
+		void compute(T alg, U startVal, boolean add, IndexedDataSource<U> data)
+	{
+		U tmp = alg.construct(startVal);
+		U one = alg.construct();
+		alg.unity().call(one);
+		long sz = data.size();
+		for (long i = 0; i < sz; i++) {
+			data.set(i, tmp);
+			if (add)
+				alg.add().call(tmp, one, tmp);
+			else
+				alg.subtract().call(tmp, one, tmp);
+		}
+	}
+}
---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestFillRamp.java
@@ -0,0 +1,90 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestFillRamp {
+
+	@Test
+	public void test1() {
+		
+		IndexedDataSource<SignedInt32Member> ints = ArrayStorage.allocateInts(
+				new int[75]);
+		SignedInt32Member tmp = G.INT32.construct();
+		SignedInt32Member startVal = G.INT32.construct();
+		startVal.setV(-13);
+		FillRamp.compute(G.INT32, startVal, true, ints);
+
+		ints.get(0, tmp);
+		assertEquals(-13, tmp.v());
+
+		ints.get(1, tmp);
+		assertEquals(-12, tmp.v());
+
+		ints.get(73, tmp);
+		assertEquals(60, tmp.v());
+
+		ints.get(74, tmp);
+		assertEquals(61, tmp.v());
+	}
+
+	@Test
+	public void test2() {
+		
+		IndexedDataSource<SignedInt32Member> ints = ArrayStorage.allocateInts(
+				new int[75]);
+		SignedInt32Member tmp = G.INT32.construct();
+		SignedInt32Member startVal = G.INT32.construct();
+		startVal.setV(45);
+		FillRamp.compute(G.INT32, startVal, false, ints);
+
+		ints.get(0, tmp);
+		assertEquals(45, tmp.v());
+
+		ints.get(1, tmp);
+		assertEquals(44, tmp.v());
+
+		ints.get(73, tmp);
+		assertEquals(-28, tmp.v());
+
+		ints.get(74, tmp);
+		assertEquals(-29, tmp.v());
+	}
+}
