c696b31b6f1dba44d69118233295e9e50bd7e69b
Barry DeZonia <bdezonia@gmail.com>
2019-08-05 20:27:20 -0500

    Define tests for the boolean conversion algorithms

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestBoolToUInt1.java
@@ -0,0 +1,67 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.bool.BooleanMember;
+import nom.bdezonia.zorbage.type.data.int1.UnsignedInt1Member;
+import nom.bdezonia.zorbage.type.storage.Storage;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestBoolToUInt1 {
+
+	@Test
+	public void test1() {
+		UnsignedInt1Member value = G.UINT1.construct();
+		IndexedDataSource<BooleanMember> bools = ArrayStorage.allocateBooleans(
+				new boolean[] {false, false, true, false, true, true});
+		IndexedDataSource<UnsignedInt1Member> ints = Storage.allocate(bools.size(), value);
+		BoolToUInt1.compute(bools, ints);
+		ints.get(0, value);
+		assertEquals(0, value.v());
+		ints.get(1, value);
+		assertEquals(0, value.v());
+		ints.get(2, value);
+		assertEquals(1, value.v());
+		ints.get(3, value);
+		assertEquals(0, value.v());
+		ints.get(4, value);
+		assertEquals(1, value.v());
+		ints.get(5, value);
+		assertEquals(1, value.v());
+	}
+}
---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestUInt1ToBool.java
@@ -0,0 +1,79 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.bool.BooleanMember;
+import nom.bdezonia.zorbage.type.data.int1.UnsignedInt1Member;
+import nom.bdezonia.zorbage.type.storage.Storage;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestUInt1ToBool {
+
+	@Test
+	public void test1() {
+		UnsignedInt1Member intValue = new UnsignedInt1Member();
+		BooleanMember boolValue = G.BOOL.construct();
+		IndexedDataSource<UnsignedInt1Member> ints = Storage.allocate(6, intValue);
+		intValue.setV(0);
+		ints.set(0, intValue);
+		intValue.setV(1);
+		ints.set(1, intValue);
+		intValue.setV(0);
+		ints.set(2, intValue);
+		intValue.setV(0);
+		ints.set(3, intValue);
+		intValue.setV(1);
+		ints.set(4, intValue);
+		intValue.setV(1);
+		ints.set(5, intValue);
+		IndexedDataSource<BooleanMember> bools = Storage.allocate(ints.size(), boolValue);
+		UInt1ToBool.compute(ints, bools);
+		bools.get(0, boolValue);
+		assertEquals(false, boolValue.v());
+		bools.get(1, boolValue);
+		assertEquals(true, boolValue.v());
+		bools.get(2, boolValue);
+		assertEquals(false, boolValue.v());
+		bools.get(3, boolValue);
+		assertEquals(false, boolValue.v());
+		bools.get(4, boolValue);
+		assertEquals(true, boolValue.v());
+		bools.get(5, boolValue);
+		assertEquals(true, boolValue.v());
+	}
+}
