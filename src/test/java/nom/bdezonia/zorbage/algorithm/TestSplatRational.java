7be0b5422e79457a54ba70f8a4f7ae7c4666aaeb
Barry DeZonia <bdezonia@gmail.com>
2019-08-06 22:05:57 -0500

    Define tests for the SpatRational algorithm

---
+++ src/test/java/nom/bdezonia/zorbage/algorithm/TestSplatRational.java
@@ -0,0 +1,63 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.tuple.Tuple2;
+import nom.bdezonia.zorbage.type.data.rational.RationalMember;
+import nom.bdezonia.zorbage.type.data.unbounded.UnboundedIntMember;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestSplatRational {
+
+	@Test
+	public void test1() {
+		RationalMember value = new RationalMember(new UnboundedIntMember(14));
+		Tuple2<BigInteger,BigInteger> tuple = new Tuple2<BigInteger,BigInteger>(null,null);
+		SplatRational.toTuple(value, tuple);
+		assertEquals(BigInteger.valueOf(14), tuple.a());
+		assertEquals(BigInteger.valueOf(1), tuple.b());
+	}
+
+	@Test
+	public void test2() {
+		Tuple2<BigInteger,BigInteger> tuple = new Tuple2<BigInteger,BigInteger>(BigInteger.valueOf(7),BigInteger.valueOf(14));
+		RationalMember value = new RationalMember();
+		SplatRational.toValue(tuple, value);
+		assertEquals(BigInteger.valueOf(1), value.n());
+		assertEquals(BigInteger.valueOf(2), value.d());
+	}
+}
