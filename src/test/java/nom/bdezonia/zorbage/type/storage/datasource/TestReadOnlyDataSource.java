cffc4ac8c5872f0118067c487fd48e443c7eaba0
Barry DeZonia <bdezonia@gmail.com>
2019-08-10 19:18:04 -0500

    Define tests for the ReadOnlyDataSource class

---
+++ src/test/java/nom/bdezonia/zorbage/type/storage/datasource/TestReadOnlyDataSource.java
@@ -0,0 +1,70 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.storage.datasource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestReadOnlyDataSource {
+
+	@Test
+	public void test1() {
+		
+		SignedInt32Member value1 = G.INT32.construct();
+		SignedInt32Member value2 = G.INT32.construct();
+
+		IndexedDataSource<SignedInt32Member> a = ArrayStorage.allocateInts(
+				new int[] {7, -99, 1, 0, 0, 121, 4000, 77, 91});
+		IndexedDataSource<SignedInt32Member> ds = new ReadOnlyDataSource<>(a);
+		
+		assertEquals(a.size(), ds.size());
+		for (int i = 0; i < ds.size(); i++) {
+			ds.get(i, value1);
+			a.get(i, value2);
+			assertEquals(value2.v(), value1.v());
+		}
+		
+		try {
+			ds.set(0, value1);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+}
