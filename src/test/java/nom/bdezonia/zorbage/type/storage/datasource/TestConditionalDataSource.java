473c6e75ef1a3b30e055676a77f4f7594fca671c
Barry DeZonia <bdezonia@gmail.com>
2019-08-10 19:09:23 -0500

    Define some tests for some of the IndexedDataSource classes

---
+++ src/test/java/nom/bdezonia/zorbage/type/storage/datasource/TestConditionalDataSource.java
@@ -0,0 +1,116 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.storage.datasource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.predicate.Predicate;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Algebra;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+import nom.bdezonia.zorbage.type.storage.array.ArrayStorage;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestConditionalDataSource {
+
+	@Test
+	public void test1() {
+
+		IndexedDataSource<SignedInt32Member> a = ArrayStorage.allocateInts(
+				new int[] {1,2,3,4,5,6,7});
+		
+		Predicate<SignedInt32Member> odd = new Predicate<SignedInt32Member>()
+		{
+			@Override
+			public boolean isTrue(SignedInt32Member value) {
+				return value.v() % 2 == 1;
+			}
+		};
+		IndexedDataSource<SignedInt32Member> odds = new ConditionalDataSource<SignedInt32Algebra, SignedInt32Member>(G.INT32, a, odd);
+		
+		SignedInt32Member value = G.INT32.construct();
+		
+		assertEquals(4, odds.size());
+		odds.get(0, value);
+		assertEquals(1, value.v());
+		odds.get(1, value);
+		assertEquals(3, value.v());
+		odds.get(2, value);
+		assertEquals(5, value.v());
+		odds.get(3, value);
+		assertEquals(7, value.v());
+
+		Predicate<SignedInt32Member> even = new Predicate<SignedInt32Member>()
+		{
+			@Override
+			public boolean isTrue(SignedInt32Member value) {
+				return value.v() % 2 == 0;
+			}
+		};
+		IndexedDataSource<SignedInt32Member> evens = new ConditionalDataSource<SignedInt32Algebra, SignedInt32Member>(G.INT32, a, even);
+		
+		assertEquals(3, evens.size());
+		evens.get(0, value);
+		assertEquals(2, value.v());
+		evens.get(1, value);
+		assertEquals(4, value.v());
+		evens.get(2, value);
+		assertEquals(6, value.v());
+		
+		value.setV(8);
+		try {
+			odds.set(3, value);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+		evens.set(0, value);
+		value.setV(0);
+		a.get(1, value);
+		assertEquals(8, value.v());
+		
+		value.setV(9);
+		try {
+			evens.set(3, value);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+		odds.set(3, value);
+		value.setV(0);
+		a.get(6, value);
+		assertEquals(9, value.v());
+	}
+}
---
+++ src/test/java/nom/bdezonia/zorbage/type/storage/datasource/TestListDataSource.java
@@ -0,0 +1,96 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.storage.datasource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.float32.real.Float32Algebra;
+import nom.bdezonia.zorbage.type.data.float32.real.Float32Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestListDataSource {
+
+	@Test
+	public void test1() {
+
+		Float32Member value = G.FLT.construct();
+		
+		List<Float32Member> a = new ArrayList<Float32Member>();
+		
+		a.add(G.FLT.construct("1"));
+		a.add(G.FLT.construct("2"));
+		a.add(G.FLT.construct("3"));
+		a.add(G.FLT.construct("4"));
+		a.add(G.FLT.construct("5"));
+
+		IndexedDataSource<Float32Member> ds = new ListDataSource<Float32Algebra, Float32Member>(G.FLT, a);
+		
+		assertEquals(5, ds.size());
+		ds.get(0, value);
+		assertEquals(1, value.v(), 0);
+		ds.get(1, value);
+		assertEquals(2, value.v(), 0);
+		ds.get(2, value);
+		assertEquals(3, value.v(), 0);
+		ds.get(3, value);
+		assertEquals(4, value.v(), 0);
+		ds.get(4, value);
+		assertEquals(5, value.v(), 0);
+		
+		try {
+			ds.get(-1, value);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		try {
+			ds.get(5, value);
+			fail();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+
+		value.setV(99);
+		
+		ds.set(4, value);
+		
+		assertEquals(99, a.get(4).v(), 0);
+	}
+}
