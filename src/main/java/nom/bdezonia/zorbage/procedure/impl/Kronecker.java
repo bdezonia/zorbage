8f1a33e0a63d602b058d9bb67011f7a53eb1f737
Barry DeZonia <bdezonia@gmail.com>
2019-08-12 00:07:08 -0500

    Define a Kronecker procedure

---
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/Kronecker.java
@@ -0,0 +1,60 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.procedure.impl;
+
+import nom.bdezonia.zorbage.procedure.Procedure3;
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Unity;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Kronecker<T extends Algebra<T,U> & Unity<U>,U>
+	implements Procedure3<Long,Long,U>
+{
+	private final T algebra;
+	private final U zero;
+	private final U one;
+	
+	public Kronecker(T algebra) {
+		this.algebra = algebra;
+		this.zero = algebra.construct();
+		this.one = algebra.construct();
+		algebra.unity().call(this.one);
+	}
+	
+	@Override
+	public void call(Long a, Long b, U c) {
+		if (a == b)
+			algebra.assign().call(one, c);
+		else
+			algebra.assign().call(zero, c);
+	}
+
+}
