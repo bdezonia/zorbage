f6a79efd9afe6e9e0d6dc684a5d590b866b02b3f
Barry DeZonia <bdezonia@gmail.com>
2019-08-14 20:49:50 -0500

    Define tests and redefine SamplingSphericalRealGrid

--- src/main/java/nom/bdezonia/zorbage/sampling/SamplingSphericalRealGrid.java
+++ src/main/java/nom/bdezonia/zorbage/sampling/SamplingSphericalRealGrid.java
@@ -40,29 +40,39 @@ import nom.bdezonia.zorbage.misc.RealUtils;
  */
 public class SamplingSphericalRealGrid implements Sampling<RealIndex> {
 
-	private final double r, dr, theta, dtheta, phi, dphi;
-	private final int rCount, thetaCount, phiCount;
+	private final SamplingGeneral<RealIndex> sampling;
 	
 	// TODO: calc me from grid cell size
 	
 	private final double TOL = 0.000001;
 	
 	public SamplingSphericalRealGrid(
-			double r, double dr, int rCount,
-			double theta, double dtheta, int thetaCount,
-			double phi, double dphi, int phiCount)
+			double dr, int rCount,
+			double dtheta, int thetaCount,
+			double dphi, int phiCount)
 	{
-		this.r = r;
-		this.dr = dr;
-		this.theta = theta;
-		this.dtheta = dtheta;
-		this.phi = phi;
-		this.dphi = dphi;
-		this.rCount = rCount;
-		this.thetaCount = thetaCount;
-		this.phiCount = phiCount;
 		if (rCount < 1 || thetaCount < 1 || phiCount < 1)
 			throw new IllegalArgumentException("counts must be >= 1 in spherical grid");
+		sampling = new SamplingGeneral<>(3);
+		RealIndex value = new RealIndex(sampling.numDimensions());
+		for (int p = 0; p < phiCount; p++) {
+			double anglePhi = p * dphi;
+			for (int th = 0; th < thetaCount; th++) {
+				double angleTheta = th * dtheta;
+				for (int r = 0; r < rCount; r++) {
+					double radius = r * dr;
+					double sineTheta = FastMath.sin(angleTheta);
+					double cosineTheta = FastMath.cos(angleTheta);
+					double sinePhi = FastMath.sin(anglePhi);
+					double cosinePhi = FastMath.cos(anglePhi);
+					// http://tutorial.math.lamar.edu/Classes/CalcIII/SphericalCoords.aspx
+					value.set(0, radius * sinePhi * cosineTheta); // xcoord
+					value.set(1, radius * sinePhi * sineTheta);  // ycoord
+					value.set(2, radius * cosinePhi);  // zcoord
+					sampling.add(value);
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -70,105 +80,22 @@ public class SamplingSphericalRealGrid implements Sampling<RealIndex> {
 		return 3;
 	}
 
-	// TODO - write tests
-	//   Not confident in definition of theta and phi from x,y,z
-	
 	@Override
 	public boolean contains(RealIndex samplePoint) {
-		if (samplePoint.numDimensions() != 3)
-			throw new IllegalArgumentException("contains() sample point does not match dimensionality");
-		double x = samplePoint.get(0);
-		double y = samplePoint.get(1);
-		double z = samplePoint.get(2);
-		double r1 = this.r;
-		double r2 = this.r + dr*rCount;
-		double r = RealUtils.distance3d(0, 0, 0, x, y, z);
-		if (r < Math.min(r1, r2) - TOL) return false;
-		if (r > Math.max(r1, r2) + TOL) return false;
-		if (!RealUtils.near((r - this.r) % dr, 0, TOL)) return false;
-		double theta = FastMath.acos(z/r);
-		double theta1 = this.theta;
-		double theta2 = this.theta + dtheta * thetaCount;
-		while (theta < 0) theta += Math.PI * 2;
-		while (theta1 < 0) theta1 += Math.PI * 2;
-		while (theta2 < 0) theta2 += Math.PI * 2;
-		if (theta1 < theta2) {
-			if (theta > theta2 + TOL) return false;
-			if (theta < theta1 - TOL) return false;
-			if (!RealUtils.near((theta - theta1) % dtheta, 0, TOL)) return false;
+		RealIndex tmp = new RealIndex(sampling.numDimensions());
+		SamplingIterator<RealIndex> iter = sampling.iterator();
+		while (iter.hasNext()) {
+			iter.next(tmp);
+			if (RealUtils.distance3d(
+					samplePoint.get(0), samplePoint.get(1), samplePoint.get(2),
+					tmp.get(0), tmp.get(1), tmp.get(2)) < TOL)
+				return true;
 		}
-		else { // theta1 > theta2 since thetaCount >= 1
-			if (theta > theta1 + TOL) return false;
-			if (theta < theta2 - TOL) return false;
-			if (!RealUtils.near((theta - theta2) % dtheta, 0, TOL)) return false;
-		}
-		double phi = FastMath.atan2(y,x);
-		double phi1 = this.phi;
-		double phi2 = this.phi + dphi*phiCount;
-		while (phi < 0) phi += Math.PI * 2;
-		while (phi1 < 0) phi1 += Math.PI * 2;
-		while (phi2 < 0) phi2 += Math.PI * 2;
-		if (phi1 < phi2) {
-			if (phi > phi2 + TOL) return false;
-			if (phi < phi1 - TOL) return false;
-			if (!RealUtils.near((phi - phi1) % dphi, 0, TOL)) return false;
-		}
-		else { // phi2 < phi1 since phiCount > 0
-			if (phi > phi1 + TOL) return false;
-			if (phi < phi2 - TOL) return false;
-			if (!RealUtils.near((phi - phi2) % dphi, 0, TOL)) return false;
-		}
-		return true;
+		return false;
 	}
 
 	@Override
 	public SamplingIterator<RealIndex> iterator() {
-		return new Iterator();
+		return sampling.iterator();
 	}
-	
-	private class Iterator implements SamplingIterator<RealIndex> {
-
-		private int tr;
-		private int ttheta;
-		private int tphi;
-		
-		private Iterator() {
-			tr = -1;
-			ttheta = 0;
-			tphi = 0;
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return !(tr == rCount-1 && ttheta == thetaCount-1 && tphi == phiCount-1);
-		}
-
-		// TODO will the origin be counted multiple times?
-		
-		@Override
-		public void next(RealIndex value) {
-			if (value.numDimensions() != 3)
-				throw new IllegalArgumentException("value does not have correct dimensions");
-			tr++;
-			if (tr >= rCount) {
-				tr = 0;
-				ttheta++;
-				if (ttheta >= thetaCount) {
-					ttheta = 0;
-					tphi++;
-					if (tphi >= phiCount)
-						throw new IllegalArgumentException("next() called when do not hasNext()");
-				}
-			}
-			final double radius = r + tr*dr;
-			final double angleTheta = theta + ttheta*dtheta;
-			final double anglePhi = phi + tphi*dphi;
-			final double s = FastMath.sin(angleTheta);
-			final double c = FastMath.cos(angleTheta);
-			value.set(0, s * FastMath.cos(anglePhi) * radius);  // xcoord
-			value.set(1, s * FastMath.sin(anglePhi) * radius);  // ycoord
-			value.set(2, c * radius);  // zcoord		}
-		}
-	}
-
 }
