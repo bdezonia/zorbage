c19bbb9535f8b3c5fa75958c6175e39030b75950
Barry DeZonia <bdezonia@gmail.com>
2019-08-17 17:48:58 -0500

    Define a bunch of tests for the predicate classes

---
+++ src/test/java/nom/bdezonia/zorbage/predicate/TestLessThanConstant.java
@@ -0,0 +1,55 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.predicate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Algebra;
+import nom.bdezonia.zorbage.type.data.int32.SignedInt32Member;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestLessThanConstant {
+
+	@Test
+	public void test() {
+		SignedInt32Member value = G.INT32.construct("17");
+		SignedInt32Member value2 = G.INT32.construct("16");
+		LessThanConstant<SignedInt32Algebra, SignedInt32Member> pred =
+				new LessThanConstant<>(G.INT32, value);
+		assertTrue(pred.isTrue(value2));
+		value2.setV(17);
+		assertFalse(pred.isTrue(value2));
+	}
+
+}
