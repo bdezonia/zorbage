d908b42f4921e807ad0a4fbb915a574ff5f1b17c
Barry DeZonia <bdezonia@gmail.com>
2019-09-16 20:11:58 -0500

    Clean up some spaces versus tabs in some example code

--- example/lorenz/Main.java
+++ example/lorenz/Main.java
@@ -33,12 +33,12 @@ public class Main extends SimpleApplication{
 	private static final float RHO = 28;
 	private static final float BETA = 8f/3;
 
-    @Override
-    public void simpleInitApp() {
-  
-    	Procedure3<Float32Member,Float32VectorMember, Float32VectorMember> lorenz =
-    			new Procedure3<Float32Member, Float32VectorMember, Float32VectorMember>()
-    	{
+	@Override
+	public void simpleInitApp() {
+
+		Procedure3<Float32Member,Float32VectorMember, Float32VectorMember> lorenz =
+				new Procedure3<Float32Member, Float32VectorMember, Float32VectorMember>()
+		{
 			@Override
 			public void call(Float32Member t, Float32VectorMember y, Float32VectorMember result) {
 				if (y.length() != 4)
@@ -64,47 +64,47 @@ public class Main extends SimpleApplication{
 				result.setV(3, v);
 			}
 		};
-    	Float32VectorMember value = G.FLT_VEC.construct();
-    	Float32Member t0 = G.FLT.construct();
-    	Float32VectorMember y0 = G.FLT_VEC.construct("[0.5,0.5,0.1,0]");
-    	int numSteps = 50000;
-    	Float32Member dt = G.FLT.construct(((Double)(1.0 / 64)).toString());
-    	IndexedDataSource<Float32VectorMember> results = ArrayDataSource.construct(G.FLT_VEC, numSteps);
-    	ClassicRungeKutta.compute(G.FLT_VEC, G.FLT, lorenz, t0, y0, numSteps, dt, results);
-        float[] xs = new float[numSteps];
-        float[] ys = new float[numSteps];
-        float[] zs = new float[numSteps];
-        float[] ts = new float[numSteps];
-        Float32Member component = G.FLT.construct();
-        for (int i = 0; i < numSteps; i++) {
-        	results.get(i, value);
-        	value.v(0, component);
-        	xs[i] = component.v();
-        	value.v(1, component);
-        	ys[i] = component.v();
-        	value.v(2, component);
-        	zs[i] = component.v();
-        	value.v(3, component);
-        	ts[i] = component.v();
-        }
-    	Node origin = new Node("origin");
-        Material mat = new Material(assetManager,
-          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
-        mat.setColor("Color", ColorRGBA.Blue);  // set color of material to blue
-        for (int i = 1; i < numSteps; i++) {
-        	Vector3f start = new Vector3f(xs[i-1], ys[i-1], zs[i-1]);
-        	Vector3f end = new Vector3f(xs[i], ys[i], zs[i]);
-            Line l = new Line(start, end);
-            Geometry geom = new Geometry("Line", l);  // create geometry from the line
-            geom.setMaterial(mat);  // set the line's material
-            origin.attachChild(geom);
-        }
-        rootNode.attachChild(origin);  // make the geometry appear in the scene
-    }
+		Float32VectorMember value = G.FLT_VEC.construct();
+		Float32Member t0 = G.FLT.construct();
+		Float32VectorMember y0 = G.FLT_VEC.construct("[0.5,0.5,0.1,0]");
+		int numSteps = 50000;
+		Float32Member dt = G.FLT.construct(((Double)(1.0 / 64)).toString());
+		IndexedDataSource<Float32VectorMember> results = ArrayDataSource.construct(G.FLT_VEC, numSteps);
+		ClassicRungeKutta.compute(G.FLT_VEC, G.FLT, lorenz, t0, y0, numSteps, dt, results);
+		float[] xs = new float[numSteps];
+		float[] ys = new float[numSteps];
+		float[] zs = new float[numSteps];
+		float[] ts = new float[numSteps];
+		Float32Member component = G.FLT.construct();
+		for (int i = 0; i < numSteps; i++) {
+			results.get(i, value);
+			value.v(0, component);
+			xs[i] = component.v();
+			value.v(1, component);
+			ys[i] = component.v();
+			value.v(2, component);
+			zs[i] = component.v();
+			value.v(3, component);
+			ts[i] = component.v();
+		}
+		Node origin = new Node("origin");
+		Material mat = new Material(assetManager,
+				"Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+		mat.setColor("Color", ColorRGBA.Blue);  // set color of material to blue
+		for (int i = 1; i < numSteps; i++) {
+			Vector3f start = new Vector3f(xs[i-1], ys[i-1], zs[i-1]);
+			Vector3f end = new Vector3f(xs[i], ys[i], zs[i]);
+			Line l = new Line(start, end);
+			Geometry geom = new Geometry("Line", l);  // create geometry from the line
+			geom.setMaterial(mat);  // set the line's material
+			origin.attachChild(geom);
+		}
+		rootNode.attachChild(origin);  // make the geometry appear in the scene
+	}
 
-    public static void main(String[] args){
-        Main app = new Main();
-        app.start();  // start the viewer
-    }
+	public static void main(String[] args){
+		Main app = new Main();
+		app.start();  // start the viewer
+	}
 
 }
