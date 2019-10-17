b29b73ff444d05e68245d84fd6b3ba5de348fe68
Barry DeZonia <bdezonia@gmail.com>
2019-10-16 19:46:12 -0500

    Define variant algorithms for BinarySearch

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/BinarySearchLeft.java
@@ -0,0 +1,75 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.type.algebra.Algebra;
+import nom.bdezonia.zorbage.type.algebra.Ordered;
+import nom.bdezonia.zorbage.type.storage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class BinarySearchLeft {
+
+	private BinarySearchLeft() { }
+	
+	/**
+	 * 
+	 * @param <T>
+	 * @param <U>
+	 * @param algebra
+	 * @param value
+	 * @param data
+	 * @return
+	 */
+	public static <T extends Algebra<T,U> & Ordered<U>, U>
+		long compute(T algebra, U value, IndexedDataSource<U> data)
+	{
+		long result = BinarySearch.compute(algebra, value, data);
+		if (result >= 0) {
+			U tmp = algebra.construct(value);
+			long idx = result;
+			long first = 0;
+			while (idx > first && algebra.isEqual().call(tmp, value)) {
+				idx -= 1;
+				data.get(idx, tmp);
+			}
+			if (idx == first) {
+				data.get(idx, tmp);
+				if (algebra.isEqual().call(tmp, value))
+					result = first;
+				else
+					result = first + 1;
+			}
+			else
+				result = idx + 1;
+		}
+		return result;
+	}
+}
