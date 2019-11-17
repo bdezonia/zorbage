da1aa5780e08608e04910a526284f2139936ab95
Barry DeZonia <bdezonia@gmail.com>
2019-11-16 22:50:24 -0600

    Define a timing test for complex math

---
+++ src/test/java/nom/bdezonia/zorbage/type/data/float64/complex/TestComplexSpeed.java
@@ -0,0 +1,72 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2019 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.type.data.float64.complex;
+
+import org.junit.Test;
+
+import nom.bdezonia.zorbage.algebras.G;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class TestComplexSpeed {
+
+	// Comparing raw speed of my code versus other libraries:
+	//   See 15 min mark of this talk: http://cds.cern.ch/record/2157242?ln=de
+	
+	private Procedure2<ComplexFloat64Member,ComplexFloat64Member> proc =
+			new Procedure2<ComplexFloat64Member, ComplexFloat64Member>()
+	{
+		@Override
+		public void call(ComplexFloat64Member a, ComplexFloat64Member b) {
+			// f(z) = z*z + (1,1)*z - 1
+			ComplexFloat64Member t1 = G.CDBL.construct();
+			ComplexFloat64Member t2 = G.CDBL.construct();
+			ComplexFloat64Member t3 = G.CDBL.construct();
+			ComplexFloat64Member linTerm = new ComplexFloat64Member(1,1);
+			G.CDBL.multiply().call(a, a, t1);
+			G.CDBL.multiply().call(linTerm, a, t2);
+			G.CDBL.unity().call(t3);
+			G.CDBL.add().call(t1, t2, t1);
+			G.CDBL.add().call(t1, t3, b);
+		}
+	};
+	
+	@Test
+	public void test1() {
+		long a = System.currentTimeMillis();
+		ComplexFloat64Member z = new ComplexFloat64Member(0.1,0);
+		for (long i = 0; i < 1000000000; i++) {
+			proc.call(z,z);
+		}
+		long b = System.currentTimeMillis();
+		System.out.println((b-a) + " millisecs");
+	}
+}
