6d11be97285511ce1e6d0c6d667eafe02798aff4
Barry DeZonia <bdezonia@gmail.com>
2020-05-16 14:29:48 -0500

    Extend the Nth algorithm to work with Accessors/Tuples

--- src/main/java/nom/bdezonia/zorbage/algorithm/Nth.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Nth.java
@@ -26,6 +26,8 @@
  */
 package nom.bdezonia.zorbage.algorithm;
 
+import nom.bdezonia.zorbage.accessor.*;
+import nom.bdezonia.zorbage.algebra.Algebra;
 import nom.bdezonia.zorbage.datasource.IndexedDataSource;
 
 /**
@@ -277,18 +279,429 @@ public class Nth {
 		data.get(19, value);
 	}
 
-        /**
-         *
-         * @param data
-         * @param value
-         * @param <U>
-         */
-        public static <U>
-        void last(IndexedDataSource<U> data, U value)
-        {
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentyfirst(IndexedDataSource<U> data, U value)
+	{
+		data.get(20, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentysecond(IndexedDataSource<U> data, U value)
+	{
+		data.get(21, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentythird(IndexedDataSource<U> data, U value)
+	{
+		data.get(22, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentyfourth(IndexedDataSource<U> data, U value)
+	{
+		data.get(23, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentyfifth(IndexedDataSource<U> data, U value)
+	{
+		data.get(24, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void twentysixth(IndexedDataSource<U> data, U value)
+	{
+		data.get(25, value);
+	}
+
+	/**
+	 *
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <U>
+	void last(IndexedDataSource<U> data, U value)
+	{
 		long sz = data.size();
 		if (sz == 0)
 			throw new IllegalArgumentException("cannot get last element from empty list");
 		data.get(sz-1, value);
-        }
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void first(T alg, AccessorA<U> data, U value)
+	{
+		alg.assign().call(data.a(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void second(T alg, AccessorB<U> data, U value)
+	{
+		alg.assign().call(data.b(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void third(T alg, AccessorC<U> data, U value)
+	{
+		alg.assign().call(data.c(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void fourth(T alg, AccessorD<U> data, U value)
+	{
+		alg.assign().call(data.d(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void fifth(T alg, AccessorE<U> data, U value)
+	{
+		alg.assign().call(data.e(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void sixth(T alg, AccessorF<U> data, U value)
+	{
+		alg.assign().call(data.f(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void seventh(T alg, AccessorG<U> data, U value)
+	{
+		alg.assign().call(data.g(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void eighth(T alg, AccessorH<U> data, U value)
+	{
+		alg.assign().call(data.h(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void ninth(T alg, AccessorI<U> data, U value)
+	{
+		alg.assign().call(data.i(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void tenth(T alg, AccessorJ<U> data, U value)
+	{
+		alg.assign().call(data.j(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void eleventh(T alg, AccessorK<U> data, U value)
+	{
+		alg.assign().call(data.k(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twelth(T alg, AccessorL<U> data, U value)
+	{
+		alg.assign().call(data.l(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void thirteenth(T alg, AccessorM<U> data, U value)
+	{
+		alg.assign().call(data.m(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void fourteenth(T alg, AccessorN<U> data, U value)
+	{
+		alg.assign().call(data.n(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void fifteenth(T alg, AccessorO<U> data, U value)
+	{
+		alg.assign().call(data.o(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void sixteenth(T alg, AccessorP<U> data, U value)
+	{
+		alg.assign().call(data.p(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void seventeenth(T alg, AccessorQ<U> data, U value)
+	{
+		alg.assign().call(data.q(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void eighteenth(T alg, AccessorR<U> data, U value)
+	{
+		alg.assign().call(data.r(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void nineteenth(T alg, AccessorS<U> data, U value)
+	{
+		alg.assign().call(data.s(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentieth(T alg, AccessorT<U> data, U value)
+	{
+		alg.assign().call(data.t(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentyfirst(T alg, AccessorU<U> data, U value)
+	{
+		alg.assign().call(data.u(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentysecond(T alg, AccessorV<U> data, U value)
+	{
+		alg.assign().call(data.v(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentythird(T alg, AccessorW<U> data, U value)
+	{
+		alg.assign().call(data.w(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentyfourth(T alg, AccessorX<U> data, U value)
+	{
+		alg.assign().call(data.x(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentyfifth(T alg, AccessorY<U> data, U value)
+	{
+		alg.assign().call(data.y(), value);
+	}
+
+	/**
+	 *
+	 * @param alg
+	 * @param data
+	 * @param value
+	 * @param <U>
+	 */
+	public static <T extends Algebra<T,U>, U>
+	void twentysixth(T alg, AccessorZ<U> data, U value)
+	{
+		alg.assign().call(data.z(), value);
+	}
+
 }
