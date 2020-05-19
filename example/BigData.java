504d77bb91afbf369455aeafa94aa4680db35e3c
Barry DeZonia <bdezonia@gmail.com>
2020-05-18 19:21:10 -0500

    Improve some example code

--- example/BigData.java
+++ example/BigData.java
@@ -1,12 +1,15 @@
 import nom.bdezonia.zorbage.algebra.G;
+import nom.bdezonia.zorbage.algorithm.Fill;
 import nom.bdezonia.zorbage.algorithm.FillSerially;
 import nom.bdezonia.zorbage.algorithm.Mean;
 import nom.bdezonia.zorbage.algorithm.Sum;
+import nom.bdezonia.zorbage.datasource.BigListDataSource;
 import nom.bdezonia.zorbage.datasource.IndexedDataSource;
 import nom.bdezonia.zorbage.datasource.ReadOnlyHighPrecisionDataSource;
 import nom.bdezonia.zorbage.storage.Storage;
 import nom.bdezonia.zorbage.type.highprec.real.HighPrecisionAlgebra;
 import nom.bdezonia.zorbage.type.highprec.real.HighPrecisionMember;
+import nom.bdezonia.zorbage.type.int16.SignedInt16Algebra;
 import nom.bdezonia.zorbage.type.int16.SignedInt16Member;
 import nom.bdezonia.zorbage.type.int16.UnsignedInt16Algebra;
 import nom.bdezonia.zorbage.type.int16.UnsignedInt16Member;
@@ -47,23 +50,59 @@ public class BigData {
 		// Now count the number of fours we found.
 		//   Notice the list is indexed by a 64-bit integer. Lists can contain up to 2^63 elements.
 
-		long numOfFours = 0;
+		long numFours = 0;
 		for (long i = 0; i < data.size(); i++) {
 			data.get(i, value);
 			if (value.v() == 4)
-				numOfFours++;
+				numFours++;
 		}
 
-		System.out.println("Number of integers with value of 4 was " + numOfFours);
+		System.out.println("Number of integers with value of 4 was " + numFours);
 	}
 
 	public void example2() {
+
+		// Another way to allocate a huge list (ALL IN RAM): 10 billion short integers (20 billion bytes).
+		//   Use the BigList class from Zorbage. The created list is zero filled. BigList based code
+		//   can allocate up to 2^62 elements; all in RAM. Actually achieving this is not realistic since
+		//   it is limited to the amount of physical RAM allocated to the JVM. But you can configure the
+		//   your JVM to use as much RAM as possible and the BigList will tap into it bypassing the
+		//   2 gig limit on the number of elements in RAM for one list.
+
+		// construct a temp variable
+
+		SignedInt16Member value = G.INT16.construct();
+
+		// allocate a BigList oriented structure
+
+		IndexedDataSource<SignedInt16Member> data =
+				new BigListDataSource<SignedInt16Algebra,SignedInt16Member>(G.INT16, 10L * 1000 * 1000 * 1000);
+
+		// Fill the list with random numbers. A memory based list will work faster with the multithreaded
+		// Fill code (the default fill).
+
+		Fill.compute(G.INT16, G.INT16.random(), data);
+
+		// Now count the number of fours we found.
+		//   Notice the list is indexed by a 64-bit integer. Lists can contain up to 2^63 elements.
+
+		long numFours = 0;
+		for (long i = 0; i < data.size(); i++) {
+			data.get(i, value);
+			if (value.v() == 4)
+				numFours++;
+		}
+
+		System.out.println("Number of integers with value of 4 was " + numFours);
+	}
+
+	public void example3() {
 
 		// One issue with working with lots of data is that doing math with many numbers can result
 		// in overflows or underflows or losses of precision. One trick Zorbage uses is to allow
 		// one to accumulate values in high precision floating point numbers. The precision of which
 		// can be set between 1 and 4000 decimal places. These numbers maintain said precision and
-		// do not over/underflow.
+		// are unbounded so they do not over/underflow.
 
 		// Allocate a 16 bit unsigned value to pass to the storage allocator
 
@@ -78,7 +117,7 @@ public class BigData {
 
 		FillSerially.compute(G.UINT16, G.UINT16.random(), data);
 
-		// Let's pull out values as high precision floating point numbers that can't lose precision. We wrap the
+		// Let's pull out values as unbounded floating point numbers that can't lose precision. We wrap the
 		// original data in a filter that reads values in the original data type and converts the result to
 		// a high precision value.
 
@@ -91,7 +130,7 @@ public class BigData {
 		HighPrecisionAlgebra.setPrecision(30); // 30 decimal places
 
 		// Create placeholders for results
-		
+
 		HighPrecisionMember sum = G.HP.construct();
 		HighPrecisionMember mean = G.HP.construct();
 
