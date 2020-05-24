d389b488a23eca54ada42e09fa455d6ef1b3ee64
Barry DeZonia <bdezonia@gmail.com>
2020-05-23 22:30:30 -0500

    Add a couple more example category placeholders

---
+++ example/SpecialFunctions.java
@@ -0,0 +1,15 @@
+
+public class SpecialFunctions {
+
+	// erf erfc
+	// fibonacci factorial
+	// permutations combinations
+	// binomial coeffs
+	// exponential calc   scinot too
+	// gaussian
+	// gcd lcm
+	// round
+	// the sincs
+	// taylor estimates
+	
+}
---
+++ example/Statistics.java
@@ -0,0 +1,6 @@
+
+public class Statistics {
+
+	// mean median min max variance stddev
+	// also the NaN versions of all these
+}
