0abe56afe95f571ddcc981ec284dbd6ffa1989c9
Barry DeZonia <bdezonia@gmail.com>
2020-05-27 22:42:45 -0500

    Add more example code

--- src/main/java/example/Quaternions.java
+++ src/main/java/example/Quaternions.java
@@ -1,1 +1,190 @@
-// discuss what can be done with them
+package example;
+
+import java.math.BigDecimal;
+
+import nom.bdezonia.zorbage.algebra.G;
+import nom.bdezonia.zorbage.algebra.GetQuaternion;
+import nom.bdezonia.zorbage.type.float16.quaternion.QuaternionFloat16Member;
+import nom.bdezonia.zorbage.type.float16.real.Float16Member;
+import nom.bdezonia.zorbage.type.float32.quaternion.QuaternionFloat32Member;
+import nom.bdezonia.zorbage.type.float32.real.Float32Member;
+import nom.bdezonia.zorbage.type.float64.quaternion.QuaternionFloat64Member;
+import nom.bdezonia.zorbage.type.float64.real.Float64Member;
+import nom.bdezonia.zorbage.type.highprec.quaternion.QuaternionHighPrecisionMember;
+import nom.bdezonia.zorbage.type.highprec.real.HighPrecisionMember;
+
+class Quaternions {
+
+	// Zorbage supports Quaternion numbers in a variety precisions
+	
+	void example1() {
+		
+		// 16-bit float based
+		
+		QuaternionFloat16Member q16 = new QuaternionFloat16Member(1.3f, -6.2f, 0, 0);
+		
+		// 32-bit float based
+		
+		QuaternionFloat32Member q32 = new QuaternionFloat32Member(99f, 44f, 1, 2);
+		
+		// 64-bit float based
+		
+		QuaternionFloat64Member q64 = new QuaternionFloat64Member(Math.E, Math.PI, -3, -7);
+		
+		// unbounded high precision float based
+		
+		QuaternionHighPrecisionMember qBig =
+				new QuaternionHighPrecisionMember(BigDecimal.valueOf(-6712),BigDecimal.valueOf(1033),
+													BigDecimal.TEN, BigDecimal.ONE);
+		
+		// use code (defined below) that can work with any of them
+		//   and use it to extract values from all kinds of Quaternion values
+		
+		@SuppressWarnings("unused")
+		Float16Member r16 = getK(G.HLF, q16);
+	
+		@SuppressWarnings("unused")
+		Float32Member r32 = getK(G.FLT, q32);
+		
+		@SuppressWarnings("unused")
+		Float64Member r64 = getK(G.DBL, q64);
+		
+		@SuppressWarnings("unused")
+		HighPrecisionMember rBig = getK(G.HP, qBig);
+	}
+	
+	<NUMBER extends GetQuaternion<COMPONENT>,
+		COMPONENT_ALGEBRA extends nom.bdezonia.zorbage.algebra.Algebra<COMPONENT_ALGEBRA,COMPONENT>,
+		COMPONENT>
+	COMPONENT getK(COMPONENT_ALGEBRA componentAlgebra, NUMBER QuaternionNumber)
+	{
+		COMPONENT component = componentAlgebra.construct();
+		QuaternionNumber.getK(component);
+		return component;
+	}
+	
+	// Zorbage can manipulate Quaternion numbers in many ways
+	
+	void example2() {
+		
+		// Any of the Quaternion algebras (G.QHLF, G.QFLT, G.QDBL, and G.QHP) implement all of
+		// the following methods.
+		
+		QuaternionFloat64Member num = G.QDBL.construct();
+		QuaternionFloat64Member other = G.QDBL.construct();
+		Float64Member val = G.DBL.construct();
+		
+		// basic java support
+		
+		num.equals(num);
+		num.hashCode();
+		num.toString();
+
+		// setters and getters
+
+		num.r();
+		num.i();
+		num.v(other);
+		num.get(other);
+		num.getR(val);
+		num.getI(val);
+		num.getJ(val);
+		num.getK(val);
+		num.setV(other);
+		num.set(other);
+		num.setR(val);
+		num.setI(val);
+		num.setJ(val);
+		num.setK(val);
+		
+		// construction
+		
+		num.allocate();  // return a new Quaternion number initialized to zero
+		num.duplicate();  // return a new Quaternion number initialized to my own values
+		
+		// more construction
+		
+		G.QDBL.construct();  // construct a zero value
+		G.QDBL.construct(num);  // construct a copy of a number
+		G.QDBL.construct("{44,77,3,2}");  // construct a Quaternion number from a string
+
+		// comparisons
+		
+		G.QDBL.isEqual();
+		G.QDBL.isNotEqual();
+		G.QDBL.isInfinite();
+		G.QDBL.isNaN();
+		G.QDBL.isZero();
+
+		// set to one or inf or nan or zero or random value
+		
+		G.QDBL.unity();
+		G.QDBL.infinite();
+		G.QDBL.nan();
+		G.QDBL.zero();
+		G.QDBL.random();
+
+		// get/use Quaternion constants
+		
+		G.QDBL.E();
+		G.QDBL.GAMMA();
+		G.QDBL.I();
+		G.QDBL.PI();
+		G.QDBL.PHI();
+
+		// basic operations
+		
+		G.QDBL.assign();
+		G.QDBL.add();
+		G.QDBL.subtract();
+		G.QDBL.multiply();
+		G.QDBL.divide();
+		G.QDBL.negate();
+		G.QDBL.invert();
+		G.QDBL.pow();
+		G.QDBL.power();
+		G.QDBL.conjugate();
+		G.QDBL.norm();
+		G.QDBL.round();
+
+		// query operations
+
+		G.QDBL.real();  // get a real number with real equal my r
+		G.QDBL.unreal(); // get a Quaternion number with real 0 and unreal equal my 0,i,j,k
+
+		// scaling
+		
+		G.QDBL.scale();
+		G.QDBL.scaleByDouble();
+		G.QDBL.scaleByHighPrec();
+		G.QDBL.scaleByRational();
+		G.QDBL.scaleComponents();
+
+		// roots
+		
+		G.QDBL.sqrt();
+		G.QDBL.cbrt();
+
+		// exp / log
+		
+		G.QDBL.exp();
+		G.QDBL.log();
+
+		// transcendentals (regular and hyperbolic)
+		
+		G.QDBL.cos();
+		G.QDBL.cosh();
+		G.QDBL.sin();
+		G.QDBL.sinh();
+		G.QDBL.tan();
+		G.QDBL.tanh();
+
+		// various sinc functions
+		
+		G.QDBL.sinc();
+		G.QDBL.sinch();
+		G.QDBL.sincpi();
+		G.QDBL.sinchpi();
+		
+	}
+}
