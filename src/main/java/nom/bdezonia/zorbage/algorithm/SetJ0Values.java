6ed2f3d71c83dcc0d0bd3cd973e64f84ce183367
Barry DeZonia <bdezonia@gmail.com>
2020-06-20 22:33:56 -0500

    Define a bunch of component access helper methods

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/SetJ0Values.java
@@ -0,0 +1,68 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (C) 2016-2020 Barry DeZonia
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.algebra.Algebra;
+import nom.bdezonia.zorbage.algebra.SetOctonion;
+import nom.bdezonia.zorbage.datasource.IndexedDataSource;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class SetJ0Values {
+
+	// do not instantiate
+	
+	private SetJ0Values() { }
+	
+	/**
+	 * 
+	 * @param algU
+	 * @param algW
+	 * @param input
+	 * @param output
+	 */
+	public static <T extends Algebra<T,U>,
+					U,
+					V extends Algebra<V,W>,
+					W extends SetOctonion<U>>
+		void compute(T algU, V algW, IndexedDataSource<U> input, IndexedDataSource<W> output)
+	{
+		Procedure2<U, W> xform = new Procedure2<U, W>() {
+			
+			@Override
+			public void call(U a, W b) {
+				b.setJ0(a);
+			}
+		};
+		
+		Transform2.compute(algU, algW, xform, input, output);
+	}
+}
