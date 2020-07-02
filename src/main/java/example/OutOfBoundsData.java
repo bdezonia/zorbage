139ee1165c6dad6841d7af1316d85394b7cb13e6
Barry DeZonia <bdezonia@gmail.com>
2020-07-02 00:12:03 -0500

    Add some more example code

--- src/main/java/example/OutOfBoundsData.java
+++ src/main/java/example/OutOfBoundsData.java
@@ -26,15 +26,303 @@
  */
 package example;
 
+import nom.bdezonia.zorbage.algebra.G;
+import nom.bdezonia.zorbage.algorithm.ConvolveND;
+import nom.bdezonia.zorbage.algorithm.FFT;
+import nom.bdezonia.zorbage.algorithm.Fill;
+import nom.bdezonia.zorbage.data.DimensionedDataSource;
+import nom.bdezonia.zorbage.data.DimensionedStorage;
+import nom.bdezonia.zorbage.data.ProcedurePaddedDimensionedDataSource;
+import nom.bdezonia.zorbage.datasource.FixedSizeDataSource;
+import nom.bdezonia.zorbage.datasource.IndexedDataSource;
+import nom.bdezonia.zorbage.datasource.ProcedurePaddedDataSource;
+import nom.bdezonia.zorbage.oob.nd.ConstantNdOOB;
+import nom.bdezonia.zorbage.oob.nd.CyclicNdOOB;
+import nom.bdezonia.zorbage.oob.nd.EdgeNdOOB;
+import nom.bdezonia.zorbage.oob.nd.MirrorNdOOB;
+import nom.bdezonia.zorbage.oob.nd.NanNdOOB;
+import nom.bdezonia.zorbage.oob.nd.ZeroNdOOB;
+import nom.bdezonia.zorbage.oob.oned.ConstantOOB;
+import nom.bdezonia.zorbage.oob.oned.CyclicOOB;
+import nom.bdezonia.zorbage.oob.oned.EdgeOOB;
+import nom.bdezonia.zorbage.oob.oned.MirrorOOB;
+import nom.bdezonia.zorbage.oob.oned.NanOOB;
+import nom.bdezonia.zorbage.oob.oned.ZeroOOB;
+import nom.bdezonia.zorbage.type.float32.complex.ComplexFloat32Member;
+import nom.bdezonia.zorbage.type.float32.real.Float32Algebra;
+import nom.bdezonia.zorbage.type.float32.real.Float32Member;
+
 /**
  * @author Barry DeZonia
  */
 class OutOfBoundsData {
 
-	// 1-d
-	// fft
+	/*
+	 * Sometimes you use an algorithm that can query data point values which are out of
+	 * the bounds of the dataset of interest Zprbage provides a mechanism for padding
+	 * datasets so that (probably neutral) values can pass through an algorithm without
+	 * corrupting the output values.
+	 */
 	
-	// n-d multidim
-	// corr / conv
+	/*
+	 * This can be apparent in 1-d datasets if you consider the case of finding the FFT
+	 * of a dataset. The current FFT algorithm requires an input dataset whose length
+	 * is a power of 2. In order to pass any size list of data to the FFT algorithm
+	 * Zorbage allows you to pad a dataset with appropriate values. then you wrap the
+	 * dataset so that any observer thinks its list's length is a power of two. Maybe
+	 * an example will be helpful here.
+	 */
+	
+	void example1() {
+		
+		// make an unusual size list
+		
+		IndexedDataSource<ComplexFloat32Member> data =
+				nom.bdezonia.zorbage.storage.Storage.allocate(413, G.CFLT.construct());
+
+		// 413
+		long weirdSize = data.size();
+		
+		// 512
+		long perfectSize = FFT.enclosingPowerOf2(weirdSize);
+		
+		// fill our input list with random data
+		
+		Fill.compute(G.CFLT, G.CFLT.random(), data);
+		
+		// now make sure querying it outside its bounds will return the value zero. the
+		// ZeroOOB procedure does the heavy lifting. the ProcedurePaddedDataSource returns
+		// values from its underlying data source when the coordinates queried are within
+		// its bounds and returns the result from a procedure call when they are out.
+		
+		IndexedDataSource<ComplexFloat32Member> paddedData =
+				new ProcedurePaddedDataSource<>(
+						G.CFLT,
+						data,
+						new ZeroOOB<>(G.CFLT, data.size()));
+		
+		// the FFT wants a power of two size. make the list have a perfect size.
+		
+		IndexedDataSource<ComplexFloat32Member> perfectSizedData =
+				new FixedSizeDataSource<>(perfectSize, paddedData);
+		
+		// allocate the destination data list
+		
+		IndexedDataSource<ComplexFloat32Member> fftData =
+				nom.bdezonia.zorbage.storage.Storage.allocate(512L, G.CFLT.construct());
+		
+		// the fft algorithm will hit every location (0 to 511). the out of bounds procedure
+		//   will return 0 when the original data is queried beyond location 412. The extra
+		//   zero values will have no effect on the final results
+		
+		FFT.compute(G.CFLT, G.FLT, perfectSizedData, fftData);
+	}
+	
+	// there are other predefined 1-d out of bounds behaviors. here is a quick tour of them.
+	
+	@SuppressWarnings("unused")
+	void example2() {
+		
+		IndexedDataSource<Float32Member> data =
+				nom.bdezonia.zorbage.storage.Storage.allocate(100, G.FLT.construct());
+
+		// pad with a constant like 0.4
+		
+		IndexedDataSource<Float32Member> constantPad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new ConstantOOB<>(G.FLT, data.size(), G.FLT.construct("0.4")));
+
+		// pad in a cyclic fashion
+		//   imagine list = [0, 1, 2, 3]
+		//   then with cyclic oob
+		//     list[3] == 3
+		//     list[4] == 0
+		//     list[5] == 1
+		//     list[6] == 2
+		//     list[7] == 3
+		//     list[8] == 0
+		//     list[9] == 1
+		
+		IndexedDataSource<Float32Member> cyclicPad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new CyclicOOB<>(data));
+		
+		// pad from the edges
+		//   imagine list = [1, 2, 3, 4]
+		//   then with edge oob
+		//     etc.
+		//     list[-2] == 1
+		//     list[-1] == 1
+		//     list[0] == 1
+		//     list[1] == 2
+		//     list[2] == 3
+		//     list[3] == 4
+		//     list[4] == 4
+		//     list[5] == 4
+		//     etc.
+		
+		IndexedDataSource<Float32Member> edgePad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new EdgeOOB<>(data));
+		
+		// pad in a mirrored fashion
+		//   imagine list = [1, 2, 3, 4]
+		//   then with mirrored oob
+		//     etc.
+		//     list[-6] == 3
+		//     list[-5] == 4
+		//     list[-4] == 4
+		//     list[-3] == 3
+		//     list[-2] == 2
+		//     list[-1] == 1
+		//     list[0] == 1
+		//     list[1] == 2
+		//     list[2] == 3
+		//     list[3] == 4
+		//     list[4] == 4
+		//     list[5] == 3
+		//     list[4] == 2
+		//     list[5] == 1
+		//     list[4] == 1
+		//     list[5] == 2
+		//     etc.
+		
+		IndexedDataSource<Float32Member> mirrorPad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new MirrorOOB<>(data));
+		
+
+		// pad with NaN
+		
+		IndexedDataSource<Float32Member> nanPad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new NanOOB<>(G.FLT, data.size()));
+		
+		// pad with 0.0
+		
+		IndexedDataSource<Float32Member> zeroPad =
+				new ProcedurePaddedDataSource<>(
+						G.FLT,
+						data,
+						new ZeroOOB<>(G.FLT, data.size()));
+
+	}
+	
+	/*
+	 * Zorbage has similar capabilities in the n-d domain rather than 1-d domain. One 
+	 * example where these become useful is when doing convolutions on n-d datasets.
+	 */
+	
+	void example3() {
+	
+		// original dims of input data
+		
+		long[] dims = new long[] {640,480};
+		
+		// make input
+		
+		DimensionedDataSource<Float32Member> input =
+				DimensionedStorage.allocate(dims, G.FLT.construct());
+		
+		// assume here we've filled it with some values
+		
+		// now pad it so id we request an out of bounds pixel we get a 0
+		
+		ProcedurePaddedDimensionedDataSource<Float32Algebra,Float32Member> paddedInput = 
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new ZeroNdOOB<>(G.FLT, input));
+		
+		// make an output dataset that has the same dims as the original dataset
+		
+		DimensionedDataSource<Float32Member> output =
+				DimensionedStorage.allocate(dims, G.FLT.construct());
+		
+		// make a 3x3 convolution filter
+		
+		DimensionedDataSource<Float32Member> filter =
+				DimensionedStorage.allocate(new long[] {3,3}, G.FLT.construct());
+		
+		// assume we've set the 8 values to something sensible
+		
+		// run a convolution. the convolve algorithm will slide the window around on the
+		// padded image and will poke outside the bounds of the original image. the padded
+		// image just returns zero for these places. and this has no effect on the computed
+		// output values.
+		
+		ConvolveND.compute(G.FLT, filter, paddedInput, output);
+	}
+	
+	// there are other predefined n-d out of bounds behaviors. here is a quick tour of them.
+	// note that their behavior matches their 1-d counterparts documented above. the
+	// following code just shows you how to construct them.
+	
+	@SuppressWarnings("unused")
+	void example4() {
+		
+		// make input
+		
+		DimensionedDataSource<Float32Member> input =
+				DimensionedStorage.allocate(new long[] {1024, 1024}, G.FLT.construct());
+		
+		// make constant padded input
+		
+		DimensionedDataSource<Float32Member> constantPad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new ConstantNdOOB<>(G.FLT, input, G.FLT.construct("1.3")));
+		
+		// make cyclic padded input
+		
+		DimensionedDataSource<Float32Member> cyclicPad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new CyclicNdOOB<>(input));
+		
+		// make edge padded input
+		
+		DimensionedDataSource<Float32Member> edgePad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new EdgeNdOOB<>(input));
+		
+		// make mirror padded input
+		
+		DimensionedDataSource<Float32Member> mirrorPad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new MirrorNdOOB<>(input));
+		
+		// make NaN padded input
+		
+		DimensionedDataSource<Float32Member> nanPad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new NanNdOOB<>(G.FLT, input));
+		
+		// make zero padded input
+		
+		DimensionedDataSource<Float32Member> zeroPad =
+				new ProcedurePaddedDimensionedDataSource<>(
+						G.FLT,
+						input,
+						new ZeroNdOOB<>(G.FLT, input));
+	}
 	
 }
