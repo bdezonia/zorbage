5bafdb4440c1d44e3c56f2b1de8205b5cf090f10
Barry DeZonia <bdezonia@gmail.com>
2020-07-17 20:56:15 -0500

    Make SummaryStats also calc the mean of the input data

--- src/main/java/nom/bdezonia/zorbage/algorithm/SummaryStats.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/SummaryStats.java
@@ -58,17 +58,18 @@ public class SummaryStats {
 	 * @param min
 	 * @param q1
 	 * @param median
+	 * @param mean
 	 * @param q3
 	 * @param max
 	 * @param noDataCount
 	 */
 	public static <T extends Algebra<T,U> & Addition<U> & Unity<U> & Ordered<U> & NaN<U>,
 					U extends Allocatable<U>>
-		void computeSafe(T alg, IndexedDataSource<U> data, U min, U q1, U median, U q3, U max, SignedInt64Member noDataCount)
+		void computeSafe(T alg, IndexedDataSource<U> data, U min, U q1, U median, U mean, U q3, U max, SignedInt64Member noDataCount)
 	{
 		IndexedDataSource<U> trimmed = NonNanValues.compute(alg, data);
 		noDataCount.setV(data.size() - trimmed.size());
-		compute(alg, trimmed, min, q1, median, q3, max);
+		compute(alg, trimmed, min, q1, median, mean, q3, max);
 	}
 	
 	/**
@@ -78,13 +79,14 @@ public class SummaryStats {
 	 * @param min
 	 * @param q1
 	 * @param median
+	 * @param mean
 	 * @param q3
 	 * @param max
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T extends Algebra<T,U> & Addition<U> & Unity<U> & Ordered<U>,
 					U extends Allocatable<U>>
-		void compute(T alg, IndexedDataSource<U> data, U min, U q1, U median, U q3, U max)
+		void compute(T alg, IndexedDataSource<U> data, U min, U q1, U median, U mean, U q3, U max)
 	{
 		IndexedDataSource<U> copy = DeepCopy.compute(alg, data);
 		
@@ -92,6 +94,11 @@ public class SummaryStats {
 		
 		long sz = copy.size();
 		
+		U sum = alg.construct();
+		U count = alg.construct();
+
+		SumWithCount.compute(alg, copy, sum, count);
+
 		U med1 = alg.construct();
 		U med2 = alg.construct();
 		
@@ -101,10 +108,10 @@ public class SummaryStats {
 		else if (sz == 1) {
 			copy.get(0, min);
 			alg.assign().call(min, q1);
-			alg.assign().call(min, median);
+			alg.assign().call(min, med1);
+			alg.assign().call(min, med2);
 			alg.assign().call(min, q3);
 			alg.assign().call(min, max);
-			return;
 		}
 		else if (sz == 2) {
 			copy.get(0, min);
@@ -116,11 +123,11 @@ public class SummaryStats {
 		}
 		else if (sz == 3) {
 			copy.get(0, min);
-			copy.get(1, median);
+			copy.get(1, med1);
 			copy.get(2, max);
+			alg.assign().call(med1, med2);
 			alg.assign().call(min, q1);
 			alg.assign().call(max, q3);
-			return;
 		}
 		else if (sz == 4) {
 			copy.get(0, min);
@@ -138,8 +145,8 @@ public class SummaryStats {
 					BigDecimal.valueOf(3).multiply(BigDecimal.valueOf(sz)).divide(BigDecimal.valueOf(4), HighPrecisionAlgebra.getContext());
 			copy.get(idx.longValue(), q3);
 			if (sz % 2 == 1) {
-				copy.get(sz/2, median);
-				return;
+				copy.get(sz/2, med1);
+				alg.assign().call(med1, med2);
 			}
 			else {
 				copy.get(sz/2-1, med1);
@@ -154,10 +161,12 @@ public class SummaryStats {
 		if (alg instanceof Invertible) {
 			Invertible<U> iAlg = (Invertible<U>) alg;
 			iAlg.divide().call(numer, denom, median);
+			iAlg.divide().call(sum, count, mean);
 		}
 		else if (alg instanceof ModularDivision) {
 			ModularDivision<U> mdAlg = (ModularDivision<U>) alg;
 			mdAlg.div().call(numer, denom, median);
+			mdAlg.div().call(sum, count, mean);
 		}
 		else {
 			throw new IllegalArgumentException("given algebra must support some kind of division operation");
