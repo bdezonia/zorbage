625d2c3e21943ba02326c375b9910c68134f7f4e
Barry DeZonia <bdezonia@gmail.com>
2020-07-20 21:18:17 -0500

    Eliminate a warning

--- src/main/java/nom/bdezonia/zorbage/procedure/impl/AverageInputs.java
+++ src/main/java/nom/bdezonia/zorbage/procedure/impl/AverageInputs.java
@@ -27,7 +27,6 @@
 package nom.bdezonia.zorbage.procedure.impl;
 
 import nom.bdezonia.zorbage.procedure.Procedure;
-import nom.bdezonia.zorbage.storage.array.ArrayStorage;
 import nom.bdezonia.zorbage.algebra.Addition;
 import nom.bdezonia.zorbage.algebra.Algebra;
 import nom.bdezonia.zorbage.algebra.Invertible;
