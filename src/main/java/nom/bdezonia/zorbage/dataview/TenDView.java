63202d4cb8fd3970f33c452e1c1545dbe7e545b5
Barry DeZonia <bdezonia@gmail.com>
2021-05-05 01:56:49 -0500

    Add a 10-d data wrapping class

---
+++ src/main/java/nom/bdezonia/zorbage/dataview/TenDView.java
@@ -0,0 +1,118 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (c) 2016-2021 Barry DeZonia All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * 
+ * Neither the name of the <copyright holder> nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package nom.bdezonia.zorbage.dataview;
+
+import nom.bdezonia.zorbage.datasource.IndexedDataSource;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ * @param <U>
+ */
+public class TenDView<U> {
+
+	private final long d0;
+	private final long d1;
+	private final long d2;
+	private final long d3;
+	private final long d4;
+	private final long d5;
+	private final long d6;
+	private final long d7;
+	private final long d8;
+	private final long d9;
+	private final IndexedDataSource<U> list;
+	
+	public TenDView(long d0, long d1, long d2, long d3, long d4, long d5, long d6, long d7, long d8, long d9, IndexedDataSource<U> data) {
+		if (d0*d1*d2*d3*d4*d5*d6*d7*d8*d9 != data.size())
+			throw new IllegalArgumentException("view dimensions do not match underlying data source dimensions");
+		this.d0 = d0;
+		this.d1 = d1;
+		this.d2 = d2;
+		this.d3 = d3;
+		this.d4 = d4;
+		this.d5 = d5;
+		this.d6 = d6;
+		this.d7 = d7;
+		this.d8 = d8;
+		this.d9 = d9;
+		this.list = data;
+	}
+	
+	public long d0() { return d0; }
+	
+	public long d1() { return d1; }
+	
+	public long d2() { return d2; }
+	
+	public long d3() { return d3; }
+	
+	public long d4() { return d4; }
+	
+	public long d5() { return d5; }
+	
+	public long d6() { return d6; }
+	
+	public long d7() { return d7; }
+	
+	public long d8() { return d8; }
+	
+	public long d9() { return d9; }
+	
+	public void get(long i0, long i1, long i2, long i3, long i4, long i5, long i6, long i7, long i8, long i9, U val) {
+		long index = i9;
+		index = index*d8 + i8;
+		index = index*d7 + i7;
+		index = index*d6 + i6;
+		index = index*d5 + i5;
+		index = index*d4 + i4;
+		index = index*d3 + i3;
+		index = index*d2 + i2;
+		index = index*d1 + i1;
+		index = index*d0 + i0;
+		list.get(index, val);
+	}
+	
+	public void set(long i0, long i1, long i2, long i3, long i4, long i5, long i6, long i7, long i8, long i9, U val) {
+		long index = i9;
+		index = index*d8 + i8;
+		index = index*d7 + i7;
+		index = index*d6 + i6;
+		index = index*d5 + i5;
+		index = index*d4 + i4;
+		index = index*d3 + i3;
+		index = index*d2 + i2;
+		index = index*d1 + i1;
+		index = index*d0 + i0;
+		list.set(index, val);
+	}
+}
