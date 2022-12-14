bfe0fd91ef8ebdd0a6f1f26050db9a317723518c
Barry DeZonia <bdezonia@wisc.edu>
2022-12-14 16:58:19 -0600

    Define some useful shortcut algorithms

---
+++ src/main/java/nom/bdezonia/zorbage/algorithm/ClampToMin.java
@@ -0,0 +1,75 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (c) 2016-2022 Barry DeZonia All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * 
+ * Neither the name of the <copyright holder> nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package nom.bdezonia.zorbage.algorithm;
+
+import nom.bdezonia.zorbage.algebra.Algebra;
+import nom.bdezonia.zorbage.algebra.Ordered;
+import nom.bdezonia.zorbage.datasource.IndexedDataSource;
+import nom.bdezonia.zorbage.procedure.Procedure2;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class ClampToMin {
+
+	// do not instantiate
+	
+	private ClampToMin() { }
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param <U>
+	 * @param algebra
+	 * @param smallestAllowed
+	 * @param input
+	 * @param output
+	 */
+	public static <T extends Algebra<T,U> & Ordered<U>, U>
+		void compute(T algebra, U smallestAllowed, IndexedDataSource<U> input, IndexedDataSource<U> output)
+	{
+		
+		Procedure2<U,U> replacer = new Procedure2<U, U>() {
+			
+			@Override
+			public void call(U a, U b) {
+				if (algebra.isLess().call(a, smallestAllowed))
+					algebra.assign().call(smallestAllowed, b);
+				else
+					algebra.assign().call(a, b);
+			}
+		}; 
+
+		Transform2.compute(algebra, replacer, input, output);
+	}
+}
