16e80265dd8b4730c9b295aa55e04b5eb7859ec6
Barry DeZonia <bdezonia@wisc.edu>
2023-01-09 16:42:30 -0600

    Add header and javadoc

--- src/main/java/nom/bdezonia/zorbage/algorithm/FlipAlongDimension.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/FlipAlongDimension.java
@@ -1,3 +1,33 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (c) 2016-2022 Barry DeZonia All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * 
+ * Neither the name of the <copyright holder> nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
 package nom.bdezonia.zorbage.algorithm;
 
 import nom.bdezonia.zorbage.algebra.Algebra;
@@ -17,12 +47,16 @@ public class FlipAlongDimension {
 	private FlipAlongDimension() { }
 
 	/**
-	 * 
+	 * Changes a data source in place. Given an axis number the data in the data source
+	 * is flipped relative to that dimension. For instance if y axis chosen then y=0 value
+	 * goes to y=max position and y=max value goes to y=0 position. All values along the
+	 * axis are "flipped" around the center position of that axis.
+	 *  
 	 * @param <T>
 	 * @param <U>
-	 * @param alg
-	 * @param dim
-	 * @param data
+	 * @param alg The algebra that can manipulate the type of values in the data source.
+	 * @param axisNumber The axis number to flip about.
+	 * @param data The data source to munge in place.
 	 */
 	public static <T extends Algebra<T,U>,U>
 		void compute(T alg, int axisNumber, DimensionedDataSource<U> data)
