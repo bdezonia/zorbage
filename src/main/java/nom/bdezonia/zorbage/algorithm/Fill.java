cf9d219992adb3ff5076a41ec3a4b18c9c0c17fb
Barry DeZonia <bdezonia@gmail.com>
2023-01-20 19:05:51 -0600

    Make Fill and Generate not read data. It is not a transform op.
    This is much more efficient than before.

--- src/main/java/nom/bdezonia/zorbage/algorithm/Fill.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Fill.java
@@ -32,8 +32,11 @@ package nom.bdezonia.zorbage.algorithm;
 
 import nom.bdezonia.zorbage.procedure.Procedure1;
 import nom.bdezonia.zorbage.procedure.impl.Constant;
+import nom.bdezonia.zorbage.tuple.Tuple2;
 import nom.bdezonia.zorbage.algebra.Algebra;
 import nom.bdezonia.zorbage.datasource.IndexedDataSource;
+import nom.bdezonia.zorbage.datasource.TrimmedDataSource;
+import nom.bdezonia.zorbage.misc.ThreadingUtils;
 
 /**
  * 
@@ -66,14 +69,87 @@ public class Fill {
 	 * a parallel algorithm to improve performance over a single threaded
 	 * implementation.
 	 * 
-	 * @param algebra
-	 * @param storage
+	 * @param algA
 	 * @param proc
+	 * @param a
 	 */
-	public static <T extends Algebra<T,U>,U>
-		void compute(T algebra, Procedure1<U> proc, IndexedDataSource<U> storage)
+	public static <AA extends Algebra<AA,A>, A>
+		void compute(AA algA, Procedure1<A> proc, IndexedDataSource<A> a)
 	{
-		Transform1.compute(algebra, proc, storage);
+		long sz = a.size();
+		
+		if (sz == 0)
+			return;
+	
+		Tuple2<Integer,Long> arrangement =
+				ThreadingUtils.arrange(a.size(),
+										a.accessWithOneThread());
+		int pieces = arrangement.a();
+		long elemsPerPiece = arrangement.b();
+		
+		if (pieces == 1) {
+			
+			Runnable r = new Computer<AA,A>(algA, proc, a);
+			r.run();
+		}
+		else {
+			
+			final Thread[] threads = new Thread[pieces];
+			long start = 0;
+			for (int i = 0; i < pieces; i++) {
+				long count;
+				if (i != pieces-1) {
+					count = elemsPerPiece;
+				}
+				else {
+					count = a.size() - start;
+				}
+				IndexedDataSource<A> aTrimmed = new TrimmedDataSource<>(a, start, count);
+				Runnable r = new Computer<AA,A>(algA, proc, aTrimmed);
+				threads[i] = new Thread(r);
+				start += count;
+			}
+	
+			for (int i = 0; i < pieces; i++) {
+				threads[i].start();
+			}
+			
+			for (int i = 0; i < pieces; i++) {
+				try {
+					threads[i].join();
+				} catch(InterruptedException e) {
+					throw new IllegalArgumentException("Thread execution error Fill");
+				}
+			}
+		}
 	}
-
+	
+	private static class Computer<AA extends Algebra<AA,A>, A>
+		implements Runnable
+	{
+		private final AA algebra;
+		private final IndexedDataSource<A> list;
+		private final Procedure1<A> proc;
+		
+		Computer(AA alg, Procedure1<A> proc, IndexedDataSource<A> lst) {
+			algebra = alg;
+			list = lst;
+			this.proc = proc;
+		}
+		
+		public void run() {
+			fillList(algebra, proc, list);
+		}
+	}
+	
+	private static <AA extends Algebra<AA,A>, A>
+		void fillList(AA algA, Procedure1<A> proc, IndexedDataSource<A> list)
+	{
+		A valueA = algA.construct();
+		final long listSize = list.size();
+		for (long i = 0; i < listSize; i++) {
+			proc.call(valueA);
+			list.set(i, valueA);
+		}
+	}
 }
--- src/main/java/nom/bdezonia/zorbage/algorithm/Generate.java
+++ src/main/java/nom/bdezonia/zorbage/algorithm/Generate.java
@@ -66,6 +66,6 @@ public class Generate {
 			}
 		};
 		
-		Transform1.compute(algU, procWrap, a);
+		Fill.compute(algU, procWrap, a);
 	}
 }
