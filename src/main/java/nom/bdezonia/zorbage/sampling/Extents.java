06b2989ae2f4f84cda20600e22666c71c674ba9a
Barry DeZonia <bdezonia@gmail.com>
2023-05-20 00:33:57 -0500

    Define some Extent calculation code in the sampling package

---
+++ src/main/java/nom/bdezonia/zorbage/sampling/Extents.java
@@ -0,0 +1,159 @@
+/*
+ * Zorbage: an algebraic data hierarchy for use in numeric processing.
+ *
+ * Copyright (c) 2016-2022 Barry DeZonia All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * 
+ * Neither the name of the <copyright holder> nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without specific
+ * prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package nom.bdezonia.zorbage.sampling;
+
+import nom.bdezonia.zorbage.tuple.Tuple2;
+
+/**
+ * 
+ * @author Barry DeZonia
+ *
+ */
+public class Extents {
+
+	// do not instantiate
+	
+	private Extents() { }
+	
+	/**
+	 * Compute the real extents of a real double sampling
+	 * 
+	 * @param sampling The set of points containing the sample
+	 * @return A tuple in the form of (min,max)
+	 */
+	public static
+		Tuple2<double[],double[]> computeDoubles(Sampling<double[]> sampling)
+	{
+		int numD = sampling.numDimensions();
+		
+		double[] min = new double[numD];
+		double[] max = new double[numD];
+		
+		for (int i = 0; i < numD; i++) {
+			min[i] = Double.MAX_VALUE;
+			max[i] = -Double.MAX_VALUE;
+		}
+		
+		double[] point = new double[numD];
+		
+		SamplingIterator<double[]> iter = sampling.iterator();
+		
+		while (iter.hasNext()) {
+			
+			iter.next(point);
+			
+			for (int i = 0; i < numD; i++) {
+				
+				if (point[i] < min[i]) min[i] = point[i];
+
+				if (point[i] > max[i]) max[i] = point[i];
+			}
+		}
+		
+		return new Tuple2<double[], double[]>(min, max);
+	}
+	
+	/**
+	 * Compute the long extents of an integral long sampling
+	 * 
+	 * @param sampling The set of points containing the sample
+	 * @return A tuple in the form of (min,max)
+	 */
+	public static
+		Tuple2<long[],long[]> computeLongs(Sampling<long[]> sampling)
+	{
+		int numD = sampling.numDimensions();
+		
+		long[] min = new long[numD];
+		long[] max = new long[numD];
+		
+		for (int i = 0; i < numD; i++) {
+			min[i] = Long.MAX_VALUE;
+			max[i] = Long.MIN_VALUE;
+		}
+		
+		long[] point = new long[numD];
+		
+		SamplingIterator<long[]> iter = sampling.iterator();
+		
+		while (iter.hasNext()) {
+			
+			iter.next(point);
+			
+			for (int i = 0; i < numD; i++) {
+				
+				if (point[i] < min[i]) min[i] = point[i];
+
+				if (point[i] > max[i]) max[i] = point[i];
+			}
+		}
+		
+		return new Tuple2<long[], long[]>(min, max);
+	}
+	
+	/**
+	 * Compute the extents of a long index sampling
+	 * 
+	 * @param sampling The set of points containing the sample
+	 * @return A tuple in the form of (min,max)
+	 */
+	public static
+		Tuple2<IntegerIndex,IntegerIndex> computeIntegerIndices(Sampling<IntegerIndex> sampling)
+	{
+		int numD = sampling.numDimensions();
+		
+		IntegerIndex min = new IntegerIndex(numD);
+		IntegerIndex max = new IntegerIndex(numD);
+		
+		for (int i = 0; i < numD; i++) {
+			min.set(i, Long.MAX_VALUE);
+			max.set(i, Long.MIN_VALUE);
+		}
+		
+		IntegerIndex point = new IntegerIndex(numD);
+		
+		SamplingIterator<IntegerIndex> iter = sampling.iterator();
+		
+		while (iter.hasNext()) {
+			
+			iter.next(point);
+			
+			for (int i = 0; i < numD; i++) {
+				
+				if (point.get(i) < min.get(i)) min.set(i, point.get(i));
+
+				if (point.get(i) > max.get(i)) max.set(i, point.get(i));
+			}
+		}
+		
+		return new Tuple2<IntegerIndex,IntegerIndex>(min, max);
+	}
+}
