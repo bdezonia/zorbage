96da32df11a0bc7cd138a4489767f0e1a87e5ba2
Barry DeZonia <bdezonia@gmail.com>
2025-09-13 20:43:09 -0500

    eliminate some warnings

--- src/test/java/nom/bdezonia/zorbage/type/integer/int15/TestSignedInt15.java
+++ src/test/java/nom/bdezonia/zorbage/type/integer/int15/TestSignedInt15.java
@@ -31,8 +31,6 @@
 package nom.bdezonia.zorbage.type.integer.int15;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Test; import nom.bdezonia.zorbage.algebra.G;
 
